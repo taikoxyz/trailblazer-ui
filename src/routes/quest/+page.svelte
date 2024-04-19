942422917d7e10eba4755fa13586cdd225160fe6
Kenk <kenghin_lim@hotmail.com>
2024-04-19 21:46:50 +0700

    feat: add cover image

---
+++ src/routes/quest/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Quest } from '$components/Quest';
+  import { Page } from '$components/Page';
+  import { CoverImage } from '$components/CoverImage';
+</script>
+
+<svelte:head>
+  <title>Taiko Trailblazer - Quest</title>
+</svelte:head>
+
+<CoverImage src={'/bg/test-cover-bg.svg'}></CoverImage>
+<Page>
+  <Quest />
+</Page>
