46985ff565beb5b6fd756f6c5382a96d1a801e82
Kenk <kenghin_lim@hotmail.com>
2024-04-20 21:04:24 +0700

    feat: add spacer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script>
   import { Trailblazer } from '$components/Trailblazer';
   import { Page } from '$components/Page';
+  import { CoverImage } from '$components/CoverImage';
 </script>
 
 <svelte:head>
   <title>Taiko Trailblazer</title>
 </svelte:head>
+
+<!-- spacer -->
+<div class="h-24"></div>
+
+<CoverImage src={''}></CoverImage>
 <Page>
   <Trailblazer />
 </Page>
