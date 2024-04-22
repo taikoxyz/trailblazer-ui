d07860250cedab39003897caa798dbda1fcdf136
Kenk <kenghin_lim@hotmail.com>
2024-04-22 21:13:32 +0700

    feat: add footer

---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+  import MediumIcon from '$images/medium-icon.svg';
+</script>
+
+<div class="w-full flex justify-center pb-[30px]">
+  <div
+    class="container flex relative bg-elevated-background bg-im rounded-3xl py-6 px-[24px] w-full box-border gap-[9px] items-center">
+    <div class="h-full grid grid-cols-5 gap-[24px] box-border w-full">
+      <div class="grid col-span-5 font-medium tracking-wider text-xs mx-auto order-0">
+        <div>JOIN THE TAIKO <span class="text-primary-brand">COMMUNITY</span></div>
+      </div>
+      <a
+        class="h-full text-secondary-content hover:text-primary-content flex items-center justify-center gap-4 px-auto lg:py-7 rounded-2xl bg-neutral border border-transparent hover:border-primary-brand"
+        href="https://discord.gg/taikoxyz"
+        target="_blank">
+        <svg width="33" height="26" viewBox="0 0 33 26" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M27.489 2.32345C25.4494 1.37425 23.2623 0.67492 20.9755 0.274385C20.9339 0.266655 20.8923 0.285973 20.8708 0.324611C20.5896 0.832039 20.278 1.49402 20.0598 2.01433C17.6003 1.64085 15.1533 1.64085 12.7442 2.01433C12.526 1.48245 12.2031 0.832039 11.9205 0.324611C11.8991 0.287262 11.8575 0.267944 11.8158 0.274385C9.53035 0.67364 7.34324 1.37297 5.30243 2.32345C5.28476 2.33118 5.26962 2.34407 5.25957 2.3608C1.11108 8.64708 -0.0253599 14.7789 0.532141 20.8346C0.534664 20.8642 0.55106 20.8926 0.573764 20.9106C3.31082 22.9493 5.96212 24.187 8.56418 25.0074C8.60583 25.0203 8.64995 25.0048 8.67645 24.9701C9.29197 24.1175 9.84065 23.2185 10.3111 22.2732C10.3389 22.2178 10.3124 22.1521 10.2556 22.1302C9.38531 21.7954 8.55661 21.3871 7.75946 20.9235C7.69641 20.8861 7.69136 20.7947 7.74936 20.7509C7.91711 20.6234 8.08491 20.4907 8.24509 20.3568C8.27407 20.3323 8.31445 20.3271 8.34852 20.3426C13.5855 22.7678 19.2551 22.7678 24.4302 20.3426C24.4643 20.3259 24.5047 20.331 24.5349 20.3555C24.6952 20.4894 24.8629 20.6234 25.0319 20.7509C25.0899 20.7947 25.0861 20.8861 25.0231 20.9235C24.2259 21.3961 23.3972 21.7954 22.5257 22.1289C22.4689 22.1508 22.4437 22.2178 22.4715 22.2732C22.952 23.2172 23.5007 24.1162 24.1048 24.9688C24.1301 25.0048 24.1755 25.0203 24.2171 25.0074C26.8318 24.187 29.4831 22.9493 32.2201 20.9106C32.2441 20.8926 32.2592 20.8655 32.2618 20.8359C32.929 13.8348 31.1442 7.75329 27.5306 2.36208C27.5217 2.34407 27.5066 2.33118 27.489 2.32345ZM11.0931 17.1473C9.51647 17.1473 8.21732 15.6791 8.21732 13.876C8.21732 12.0729 9.49127 10.6047 11.0931 10.6047C12.7076 10.6047 13.9942 12.0858 13.9689 13.876C13.9689 15.6791 12.695 17.1473 11.0931 17.1473ZM21.726 17.1473C20.1494 17.1473 18.8502 15.6791 18.8502 13.876C18.8502 12.0729 20.1241 10.6047 21.726 10.6047C23.3405 10.6047 24.627 12.0858 24.6018 13.876C24.6018 15.6791 23.3405 17.1473 21.726 17.1473Z"
+            fill="currentColor" />
+        </svg>
+        <div class="social-media-text hidden lg:flex text-primary-content">discord</div>
+      </a>
+      <a
+        class="text-secondary-content hover:text-primary-content flex items-center justify-center gap-4 px-auto py-5 lg:py-7 rounded-2xl bg-neutral border border-transparent hover:border-primary-brand"
+        href="https://www.twitter.com/taikoxyz"
+        target="_blank">
+        <svg width="32" height="29" viewBox="0 0 32 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M25.2624 0.352661H30.0415L19.6006 12.2859L31.8835 28.5245H22.266L14.7333 18.6759L6.11422 28.5245H1.33224L12.4998 15.7605L0.716797 0.352661H10.5784L17.3873 9.35463L25.2624 0.352661ZM23.5851 25.6639H26.2332L9.13944 3.06293H6.2977L23.5851 25.6639Z"
+            fill="currentColor" />
+        </svg>
+
+        <div class="social-media-text hidden lg:flex text-primary-content">twitter</div>
+      </a>
+      <a
+        class="text-secondary-content hover:text-primary-content flex items-center justify-center gap-4 px-auto lg:py-7 rounded-2xl bg-neutral border border-transparent hover:border-primary-brand"
+        href="https://taiko.mirror.xyz/"
+        target="_blank">
+        <img class="size-[25px] lg:scale-100" src={MediumIcon} alt="mirror" />
+        <div class="social-media-text hidden lg:flex text-primary-content">mirror</div>
+      </a>
+      <a
+        class="text-secondary-content hover:text-primary-content flex items-center justify-center gap-4 px-auto lg:py-7 rounded-2xl bg-neutral border border-transparent hover:border-primary-brand"
+        href="https://community.taiko.xyz/"
+        target="_blank">
+        <svg width="34" height="31" viewBox="0 0 34 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M33.1552 24.8897L27.4115 17.0862C26.7876 16.2382 25.8636 15.7302 24.8797 15.6182C24.6557 15.5922 24.4557 15.4623 24.3437 15.2663C24.2297 15.0703 24.2177 14.8323 24.3077 14.6243C24.7017 13.7164 24.7257 12.6624 24.3017 11.6985L20.4159 2.82302C19.84 1.5051 18.5381 0.655151 17.1001 0.655151C15.6622 0.655151 14.3603 1.5071 13.7843 2.82302L9.89858 11.6985C9.47661 12.6624 9.4986 13.7164 9.89258 14.6243C9.98258 14.8323 9.96858 15.0703 9.85658 15.2663C9.74259 15.4623 9.5446 15.5922 9.32061 15.6182C8.33667 15.7302 7.41273 16.2382 6.78877 17.0862L1.04512 24.8897C0.193167 26.0476 0.107171 27.5995 0.825128 28.8455C1.54508 30.0894 2.933 30.7913 4.36091 30.6334L13.9903 29.5614C15.0363 29.4454 15.9382 28.8994 16.5262 28.1035C16.6602 27.9215 16.8742 27.8135 17.1001 27.8135C17.3261 27.8135 17.5381 27.9215 17.6741 28.1035C18.2621 28.8994 19.164 29.4454 20.21 29.5614L29.8394 30.6334C31.2673 30.7933 32.6552 30.0914 33.3752 28.8455C34.0931 27.5995 34.0071 26.0476 33.1552 24.8897ZM12.4104 12.8124L16.3022 3.92695C16.4422 3.60897 16.7562 3.40298 17.1021 3.40298C17.4481 3.40298 17.7621 3.60897 17.9021 3.92695L21.7939 12.8124C21.9159 13.0924 21.8899 13.4164 21.7219 13.6724C21.5539 13.9283 21.2699 14.0823 20.9619 14.0823H13.2404C12.9344 14.0823 12.6484 13.9283 12.4804 13.6724C12.3124 13.4164 12.2864 13.0924 12.4084 12.8124H12.4104ZM14.4083 26.3396C14.2703 26.6136 14.0023 26.7976 13.6984 26.8316L4.05893 27.9035C3.71495 27.9415 3.37897 27.7735 3.20498 27.4735C3.03099 27.1736 3.05299 26.7996 3.25898 26.5196L9.00863 18.7081C9.19062 18.4621 9.48261 18.3221 9.78859 18.3401C10.0946 18.3561 10.3706 18.5261 10.5245 18.7921L10.5305 18.8021L14.3803 25.4697L14.3863 25.4797C14.5403 25.7456 14.5483 26.0696 14.4103 26.3416L14.4083 26.3396ZM17.8921 23.3238C17.7281 23.6058 17.4281 23.7818 17.1001 23.7818C16.7742 23.7818 16.4722 23.6078 16.3082 23.3258L13.3504 18.2041C13.1864 17.9221 13.1864 17.5721 13.3504 17.2902C13.5144 17.0082 13.8143 16.8322 14.1423 16.8322H20.056C20.382 16.8322 20.6839 17.0042 20.8479 17.2881C21.0119 17.5721 21.0119 17.9201 20.8479 18.2021L17.8921 23.3238ZM30.9973 27.4735C30.8233 27.7735 30.4893 27.9435 30.1434 27.9055L20.5039 26.8336C20.2 26.7996 19.932 26.6156 19.794 26.3416C19.656 26.0676 19.664 25.7436 19.818 25.4797L19.824 25.4697L23.6738 18.8021L23.6798 18.7921C23.8337 18.5261 24.1097 18.3561 24.4157 18.3401C24.7217 18.3241 25.0137 18.4621 25.1957 18.7081L30.9453 26.5196C31.1513 26.7996 31.1713 27.1736 30.9993 27.4735H30.9973Z"
+            fill="currentColor" />
+        </svg>
+
+        <div class="social-media-text hidden lg:flex text-primary-content">forum</div>
+      </a>
+      <a
+        class="text-secondary-content hover:text-primary-content flex items-center justify-center gap-4 px-auto lg:py-7 rounded-2xl bg-neutral border border-transparent hover:border-primary-brand"
+        href="https://www.youtube.com/@taikoxyz"
+        target="_blank">
+        <svg width="36" height="25" viewBox="0 0 36 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M35.3516 5.65649C35.3516 5.65649 35.0098 3.24341 33.957 2.18384C32.624 0.789307 31.1338 0.782471 30.4502 0.700439C25.5557 0.344971 18.207 0.344971 18.207 0.344971H18.1934C18.1934 0.344971 10.8447 0.344971 5.9502 0.700439C5.2666 0.782471 3.77637 0.789307 2.44336 2.18384C1.39063 3.24341 1.05566 5.65649 1.05566 5.65649C1.05566 5.65649 0.700195 8.49341 0.700195 11.3235V13.9758C0.700195 16.8059 1.04883 19.6428 1.04883 19.6428C1.04883 19.6428 1.39062 22.0559 2.43652 23.1155C3.76953 24.51 5.51953 24.4622 6.29883 24.6125C9.10156 24.8792 18.2002 24.9612 18.2002 24.9612C18.2002 24.9612 25.5557 24.9475 30.4502 24.5989C31.1338 24.5168 32.624 24.51 33.957 23.1155C35.0098 22.0559 35.3516 19.6428 35.3516 19.6428C35.3516 19.6428 35.7002 16.8127 35.7002 13.9758V11.3235C35.7002 8.49341 35.3516 5.65649 35.3516 5.65649ZM14.584 17.1956V7.35864L24.0381 12.2942L14.584 17.1956Z"
+            fill="currentColor" />
+        </svg>
+
+        <div class="social-media-text hidden lg:flex text-primary-content">youtube</div>
+      </a>
+
+      <div
+        class="flex flex-col col-span-5 lg:gap-4 lg:py-0 gap-2 py-4 lg:col-span-1 order-4 lg:order-1 justify-center text-center lg:text-left lg:m-0">
+        <div class="font-clash-grotesk display-small-medium text-[36px]/[40px]">Taiko</div>
+        <div class="body-regular text-secondary-content">
+          The most developer-friendly and secure Ethereum scaling solution
+        </div>
+      </div>
+      <div
+        class="flex flex-col lg:flex-row gap-8 lg:gap-16 grid-cols-1 bg-neutral lg:grid-cols-4 col-span-5 lg:col-span-4 w-full h-full rounded-2xl box-border order-3 bg-elevated-background px-[45px] py-[40px]">
+        <div class="flex flex-col gap-4">
+          <div class="body-bold">ABOUT</div>
+          <a href="https://taiko.mirror.xyz/" class="body-regular text-secondary-content hover:text-primary-content"
+            >Blog</a>
+          <a
+            href="https://taikoxyz.notion.site/Taiko-Jobs-828fd7232d2c4150a11e10c8baa910a2"
+            class="body-regular text-secondary-content hover:text-primary-content">Careers</a>
+          <a
+            href="https://github.com/taikoxyz/taiko-mono/tree/main/packages/branding/"
+            class="body-regular text-secondary-content hover:text-primary-content">Brand kit</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <div class="body-bold">DEVELOPERS</div>
+          <a
+            href="https://docs.taiko.xyz/start-here/getting-started"
+            class="body-regular text-secondary-content hover:text-primary-content">Get started</a>
+          <a href="https://github.com/taikoxyz" class="body-regular text-secondary-content hover:text-primary-content"
+            >Github</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <div class="body-bold">SOLUTIONS</div>
+          <a
+            href="https://bridge.hekla.taiko.xyz/"
+            class="body-regular text-secondary-content hover:text-primary-content">Bridge</a>
+          <a href="https://swap.hekla.taiko.xyz/" class="body-regular text-secondary-content hover:text-primary-content"
+            >Swap</a>
+          <a href="https://docs.taiko.xyz/" class="body-regular text-secondary-content hover:text-primary-content"
+            >Documentation</a>
+          <a
+            href="https://explorer.hekla.taiko.xyz"
+            class="body-regular text-secondary-content hover:text-primary-content">Explorer</a>
+        </div>
+        <div class="col-span-5 lg:col-span-1 opacity-25 mt-auto ml-auto text-right body-regular order-7">
+          © 2024 Taiko Labs
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
