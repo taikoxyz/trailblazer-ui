ad54d54c5d864830afd1422907f4d24fd9bb7edd
Kenk <kenghin_lim@hotmail.com>
2024-04-26 18:06:47 +0700

    fix: fixed layout

--- src/components/Profile/ProfileBoosters.svelte
+++ src/components/Profile/ProfileBoosters.svelte
@@ -6,8 +6,18 @@
   let boosters = MOCK_USER_BOOSTERS;
 </script>
 
-<div class="grid grid-cols-4 gap-5">
+<div class="box">
   {#each boosters as booster}
     <BoosterItem data={booster} />
   {/each}
 </div>
+
+<style>
+  /* 306px is the size of the card bg */
+  .box {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(306px, 1fr));
+    gap: 10px;
+    margin: 20px;
+  }
+</style>
