98790ba63444dd90abf5ebe28919b60e58d75b99
Kenk <kenghin_lim@hotmail.com>
2024-04-29 11:18:30 +0700

    Refactor FactionBadgeItem, FactionBadges, and FactionImage components in Profile folder

---
+++ src/components/Profile/FactionBadges.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { MOCK_USER_FACTIONS } from 'src/tests/mocks/userFactions';
+  import FactionBadgeItem from './FactionBadgeItem.svelte';
+
+  let factions = MOCK_USER_FACTIONS;
+</script>
+
+<div class="box">
+  {#each factions as faction}
+    <FactionBadgeItem data={faction} />
+  {/each}
+</div>
+
+<style>
+  /* 306px is the size of the card bg */
+  .box {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(306px, 1fr));
+    gap: 10px;
+    margin: 20px;
+  }
+</style>
---
+++ src/components/Profile/FactionImage.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import type { Faction } from '$libs/profile';
+
+  import RaverImage from '$images/factions/faction-1.png';
+  import RobotsImage from '$images/factions/faction-2.png';
+  // import AndroidsImage from '$images/factions/androids.svg';
+  // import BouncerImage from '$images/factions/bouncers.svg';
+  // import DrummerImage from '$images/factions/drummers.svg';
+  // import MasterImage from '$images/factions/masters.svg';
+  // import MonkImage from '$images/factions/monks.svg';
+  // import ShintoImage from '$images/factions/shinto.svg';
+
+  export let type: Faction;
+</script>
+
+<!-- {#if type === 'Ravers'}
+  <img src={RaverImage} alt={RaverImage} />
+{:else if type === 'Robots'}
+  <img src={RobotsImage} alt={RobotsImage} />
+{:else if type === 'Bouncers'}
+  <img src={BouncerImage} alt={BouncerImage} />
+{:else if type === 'Masters'}
+  <img src={MasterImage} alt={MasterImage} />
+{:else if type === 'Monks'}
+  <img src={MonkImage} alt={MonkImage} />
+{:else if type === 'Drummers'}
+  <img src={DrummerImage} alt={DrummerImage} />
+{:else if type === 'Androids'}
+  <img src={AndroidsImage} alt={AndroidsImage} />
+{:else if type === 'Shinto'}
+  <img src={ShintoImage} alt={ShintoImage} />
+{:else}
+  💩💩💩
+{/if} -->
+
+{#if type === 'Ravers'}
+  <img src={RaverImage} alt={RaverImage} />
+{:else if type === 'Robots'}
+  <img src={RobotsImage} alt={RobotsImage} />
+{:else if type === 'Bouncers'}
+  bouncers
+{:else if type === 'Masters'}
+  masters
+{:else if type === 'Monks'}
+  monks
+{:else if type === 'Drummers'}
+  drummers
+{:else if type === 'Androids'}
+  androids
+{:else if type === 'Shinto'}
+  shinto
+{:else}
+  💩💩💩
+{/if}
