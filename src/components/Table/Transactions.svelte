bd826da5a8378c5e4b9a10632cfa218f45002014
Kenk <kenghin_lim@hotmail.com>
2024-04-30 14:22:40 +0700

    Refactor point history rendering in Transactions.svelte

--- src/components/Table/Transactions.svelte
+++ src/components/Table/Transactions.svelte
@@ -24,16 +24,18 @@
   <div class="overflow-y-scroll max-h-[529px] block bg-elevated-background px-[5px]">
     <table class="table w-full border-collapse bg-elevated-background">
       <tbody class="border-none pt-6 overflow-scroll">
-        {#each $currentProfile.pointsHistory.items as pointHistory}
-          <tr class="border-2 border-transparent hover:border-2 hover:border-orange-400">
-            <td class="flex gap-2 items-center">
-              <Skeleton width="w-4" height="h-4" bgColor="bg-blue-200" shineColor="bg-blue-100" />
-              {pointHistory?.event}
-            </td>
-            <td>{pointHistory?.points}</td>
-            <td>{new Date(pointHistory?.date * 1000).toLocaleString()}</td>
-          </tr>
-        {/each}
+        {#if $currentProfile.pointsHistory}
+          {#each $currentProfile.pointsHistory?.items as pointHistory}
+            <tr class="border-2 border-transparent hover:border-2 hover:border-orange-400">
+              <td class="flex gap-2 items-center">
+                <Skeleton width="w-4" height="h-4" bgColor="bg-blue-200" shineColor="bg-blue-100" />
+                {pointHistory?.event}
+              </td>
+              <td>{pointHistory?.points}</td>
+              <td>{new Date(pointHistory?.date * 1000).toLocaleString()}</td>
+            </tr>
+          {/each}
+        {/if}
       </tbody>
     </table>
   </div>
