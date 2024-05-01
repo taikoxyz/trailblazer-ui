3b029795035a6bcc8aea17f871ed1a025cbc4f00
Kenk <kenghin_lim@hotmail.com>
2024-05-01 16:00:43 +0700

    feat: add Bar and CountUp

---
+++ src/components/Numbers/CountUp.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  // Count up to value using svelte spring
+  import { spring } from 'svelte/motion';
+
+  export let value = 42069;
+  let countSpring = spring(0, { stiffness: 0.04, damping: 1, precision: 0.001 });
+
+  countSpring.update(() => value);
+</script>
+
+<div class={$$props.class}>{$countSpring.toFixed(0)}</div>
