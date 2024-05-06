1cd3112e389b8c5fd206a4fb5331b3bba786b2c2
Kenk <kenghin_lim@hotmail.com>
2024-05-06 21:24:23 +0700

    feat: remove trailblazer imports

--- src/components/Trailblazer/Trailblazer.svelte
+++ src/components/Trailblazer/Trailblazer.svelte
@@ -1,7 +1,4 @@
 <script>
-  import { ShinyCard } from '$components/Card';
-  import Carousel from '$components/Carousel/Carousel.svelte';
-  import LandingTitle from '$components/Landing/LandingTitle.svelte';
 </script>
 
 <div class="flex flex-col w-full items-center gap-8"></div>
