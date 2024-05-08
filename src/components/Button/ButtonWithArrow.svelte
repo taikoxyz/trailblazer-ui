275439ee2812693abebb4230db093f95e75dbfd4
Kenk <kenghin_lim@hotmail.com>
2024-05-08 22:35:00 +0700

    feat: add ButtonWithArrow component

---
+++ src/components/Button/ButtonWithArrow.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Icon } from '$components/Icon';
+
+  export let text: string = 'Learn more';
+</script>
+
+<button class="f-center pl-5 pr-2 py-2 rounded-full bg-secondary-brand gap-[12px]">
+  <div class="text-white body-medium">{text}</div>
+  <div class="f-center size-8 rounded-full bg-white overflow-hidden">
+    <div class="flex gap-[14px] translate-x-[-14px] hover:animate-slide-right">
+      <Icon type={'arrow-right'} fillClass="fill-secondary-brand" width={14} />
+      <Icon type={'arrow-right'} fillClass="fill-secondary-brand" width={14} />
+    </div>
+  </div>
+</button>
