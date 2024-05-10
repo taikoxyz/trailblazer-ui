075ffdfa958e10fdc5e771479d89e557cea34c31
Kenk <kenghin_lim@hotmail.com>
2024-05-10 10:02:49 +0700

    feat: Add startWatchingX and stopWatchingX functions to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
   import { SwitchChainModal } from '$components/SwitchChainModal';
   import { WelcomeModal } from '$components/WelcomeModal';
   import { startWatching, stopWatching } from '$libs/wagmi';
+  import { startWatching as startWatchingX, stopWatching as stopWatchingX } from '$libs/supabase';
   import { Footer } from '$components/Footer';
   import { Ribbon } from '$components/Ribbon';
 
@@ -22,11 +23,13 @@
 
   onMount(async () => {
     await startWatching();
+    startWatchingX();
     document.body.addEventListener('pointermove', syncPointer);
   });
 
   onDestroy(() => {
     stopWatching();
+    stopWatchingX();
     document.body.removeEventListener('pointermove', syncPointer);
   });
 </script>
