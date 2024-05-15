c3d5a32a863815f8d99192347945de607eebf56f
Kenk <kenghin_lim@hotmail.com>
2024-05-15 13:11:43 +0700

    refactor: FAQ Section

---
+++ src/components/FAQ/FAQ.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { FAQDropdown } from '$components/Dropdown';
+  import { MOCK_FAQS } from '../../tests/mocks/faq';
+</script>
+
+<div class=" w-full px-[0px] py-[100px]">
+  <div class="pb-[30px]">
+    <div class="display-large-medium">FAQs</div>
+  </div>
+  <div class="flex flex-col w-full gap-4">
+    {#each MOCK_FAQS as { question, answer }}
+      <FAQDropdown {question} {answer}></FAQDropdown>
+    {/each}
+  </div>
+</div>
