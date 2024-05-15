7d5f39467431e3aa8e8df18a449e69f8e0677dc1
Kenk <kenghin_lim@hotmail.com>
2024-05-15 16:20:21 +0700

    feat: factions gallery - mobile

--- src/components/About/AboutGallery.svelte
+++ src/components/About/AboutGallery.svelte
@@ -3,26 +3,27 @@
 </script>
 
 <!-- Title Gallery -->
-<div class="flex gap-3 w-full">
-  <div class="rounded-[30px] max-h-[745px] min-w-[530px] overflow-hidden">
+<div class="flex flex-col px-5 xl:flex-row gap-5 xl:gap-3 w-full">
+  <div class="rounded-[30px] max-h-[745px] xl:min-w-[530px] overflow-hidden">
     <img class="rounded-[30px] w-full hover-zoom" src={GalleryImage} alt={GalleryImage} />
   </div>
-  <div class="flex flex-col gap-3 w-full">
+  <div class="flex flex-col gap-5 xl:gap-3 w-full">
     <div class="flex flex-col h-full w-full rounded-[30px] bg-elevated-background px-[33px] py-[36px]">
       <div class="body-bold text-primary-brand tracking-[0.175px] text-[14px]/[20px] pb-[33px]">
         WELCOME TO NEO NAZUKA
       </div>
-      <div class="font-clash-grotesk tracking-[-1.5px] text-[75px]/[70px] pb-[116px]">
+      <div
+        class="font-clash-grotesk tracking-[-1.5px] text-[45px]/[52px] xl:text-[75px]/[70px] pb-[50px] xl:pb-[116px]">
         The Dawn of<br />the 'Based'<br /> Revolution.
       </div>
-      <div class="text-secondary-content text-[18px]/[24px] max-w-[535px] max-h-[120px]">
+      <div class="text-secondary-content text-[18px]/[24px] max-w-[535px] xl:max-h-[120px]">
         Embark on a journey through the pre-mainnet challenges of Trailblazers: The Call of Taiko and earn your place
         among the elite. Each challenge you complete allows you to collect exclusive badges from the Taiko Factions,
         which are more than mere collectibles.
       </div>
     </div>
     <div class="h-fit w-full rounded-[30px] bg-pink-400 px-[33px] py-[36px]">
-      <div class="font-clash-grotesk text-base-content tracking-[-1.5px] text-[45px]/[45px]">
+      <div class="font-clash-grotesk text-base-content tracking-[-1.5px] text-[35px]/[42px] xl:text-[45px]/[45px]">
         Taiko needs you.<br /><span class="">Will you answer the call?</span>
       </div>
     </div>
