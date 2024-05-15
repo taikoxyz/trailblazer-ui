2ea5eb98b0f07d58eda84c2a209134fcafe1f08c
Kenk <kenghin_lim@hotmail.com>
2024-05-15 13:18:31 +0700

    feat: create about page

---
+++ src/components/About/About.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { ResponsiveVideo } from '$components/Video';
+  import AboutGallery from './AboutGallery.svelte';
+</script>
+
+<div class="f-center container flex-col w-full overflow-visible">
+  <!-- About Gallery -->
+  <AboutGallery />
+
+  <div class="flex flex-col w-full py-[86px] gap-[132px]">
+    <ResponsiveVideo />
+  </div>
+
+  <!-- faq section -->
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { About } from '$components/About';
+</script>
+
+<svelte:head>
+  <title>Taiko Trailblazer - About</title>
+</svelte:head>
+
+<!-- spacer -->
+<div class="h-24"></div>
+
+<div class="f-center px-8">
+  <About />
+</div>
