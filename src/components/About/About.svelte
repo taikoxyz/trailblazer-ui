f636e7d5adf298d5a9258358efc5c9134ae25457
Kenk <kenghin_lim@hotmail.com>
2024-05-15 14:06:23 +0700

    feat: add FAQ component to About page

--- src/components/About/About.svelte
+++ src/components/About/About.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { FAQ } from '$components/FAQ';
   import { FactionsGallery } from '$components/Gallery';
   import { ResponsiveVideo } from '$components/Video';
   import AboutFactionsInfo from './AboutFactionsInfo.svelte';
@@ -8,14 +9,12 @@
 <div class="f-center container flex-col w-full overflow-visible gap-[120px]">
   <!-- About Gallery -->
   <AboutGallery />
-
+  <!-- Factions Gallery -->
   <FactionsGallery />
-  <!--  -->
+  <!-- About Factions -->
   <AboutFactionsInfo />
-
-  <div class="flex flex-col w-full py-[86px] gap-[132px]">
-    <ResponsiveVideo />
-  </div>
-
-  <!-- faq section -->
+  <!-- Video -->
+  <ResponsiveVideo />
+  <!-- FAQ -->
+  <FAQ />
 </div>
