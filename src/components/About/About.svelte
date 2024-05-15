38c41e9fdba9015c3778cd351ea15191499794a1
Kenk <kenghin_lim@hotmail.com>
2024-05-15 14:03:16 +0700

    feat: improve About page layout and add FactionsGallery

--- src/components/About/About.svelte
+++ src/components/About/About.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import { FactionsGallery } from '$components/Gallery';
   import { ResponsiveVideo } from '$components/Video';
   import AboutGallery from './AboutGallery.svelte';
 </script>
 
-<div class="f-center container flex-col w-full overflow-visible">
+<div class="f-center container flex-col w-full overflow-visible gap-[120px]">
   <!-- About Gallery -->
   <AboutGallery />
+
+  <FactionsGallery />
 
   <div class="flex flex-col w-full py-[86px] gap-[132px]">
     <ResponsiveVideo />
