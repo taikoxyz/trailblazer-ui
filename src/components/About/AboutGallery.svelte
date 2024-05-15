899f2d51461ed3ec327ee137cb130978b59c014b
Kenk <kenghin_lim@hotmail.com>
2024-05-15 14:01:28 +0700

    feat: update AboutGallery style

--- src/components/About/AboutGallery.svelte
+++ src/components/About/AboutGallery.svelte
@@ -6,21 +6,21 @@
   <div class="rounded-[30px] bg-pink-400 min-w-[530px]"></div>
   <div class="flex flex-col gap-3 w-full">
     <div class="flex flex-col h-full w-full rounded-[30px] bg-elevated-background px-[33px] py-[36px]">
-      <div class="body-bold text-secondary-content tracking-[0.175px] text-[14px]/[20px] pb-[33px]">
-        STEP-BY-STEP GUIDE
+      <div class="body-bold text-primary-brand tracking-[0.175px] text-[14px]/[20px] pb-[33px]">
+        WELCOME TO NEO NAZUKA
       </div>
       <div class="font-clash-grotesk tracking-[-1.5px] text-[75px]/[70px] pb-[116px]">
         The Dawn of<br />the 'Based'<br /> Revolution.
       </div>
-      <div class="text-[18px]/[24px] max-w-[535px] max-h-[120px]">
+      <div class="text-secondary-content text-[18px]/[24px] max-w-[535px] max-h-[120px]">
         Embark on a journey through the pre-mainnet challenges of Trailblazers: The Call of Taiko and earn your place
         among the elite. Each challenge you complete allows you to collect exclusive badges from the Taiko Factions,
         which are more than mere collectibles.
       </div>
     </div>
     <div class="h-fit w-full rounded-[30px] bg-pink-400 px-[33px] py-[36px]">
-      <div class="font-clash-grotesk text-base-content tracking-[-1.5px] text-[75px]/[70px]">
-        ring ring mf Taiko needs you.<br /><span class="">pick up the phone gdi</span>
+      <div class="font-clash-grotesk text-base-content tracking-[-1.5px] text-[45px]/[45px]">
+        Taiko needs you.<br /><span class="">Will you answer the call?</span>
       </div>
     </div>
   </div>
