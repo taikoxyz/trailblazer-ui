77c559816ce3694bcb4f65f4992c8e9b2d7c544e
Kenk <kenghin_lim@hotmail.com>
2024-05-17 19:10:23 +0700

    wip

--- src/routes/pinkify/render/[slug]/+server.ts
+++ src/routes/pinkify/render/[slug]/+server.ts
@@ -1,64 +1,59 @@
-
 import { error } from '@sveltejs/kit';
+import { createCanvas, loadImage } from 'canvas';
 
 import { generateTwitterCardSVG } from '$libs/pinkify/generateTwitterCard.js';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-
-
   // Extract slug parameter from the request
   const { pathname } = url;
 
-  // split the pathname to get the slug
+  // Split the pathname to get the slug
   const slug = pathname.split('/').pop();
+
   // Verify if slug exists
   if (!slug) {
-    return error(400, 'Bad Request');
+    return new Response('Bad Request', { status: 400 });
   }
 
-
-
   try {
-    // Fetch the image from twitter
+    // Fetch the image from Twitter
     const response = await fetch(`https://pbs.twimg.com/profile_images/${slug}/X926izfy_400x400.jpg`);
-    // 1773273774345138176
 
     if (!response.ok) {
-      return error(response.status, `Failed to fetch image: ${response.statusText}`);
+      return new Response(`Failed to fetch image: ${response.statusText}`, { status: response.status });
     }
 
+    // Get Image
     const blob = await response.blob();
-
-    // const base64data = await blobToBase64(blob);
-    // const svg = `<svg
-    // id="test"
-    // width="600"
-    // viewBox="0 0 1200 600"
-    // preserveAspectRatio="xMidYMid meet"
-    // xmlns="http://www.w3.org/2000/svg">
-    // <rect x="0" y="0" width="100%" height="100%" fill="#d63b95" />
-    // <image x="50%" y="50%" width="300" height="400" transform="translate(-150,-150)" href="${base64data}" />
-    // <text
-    //   x="50%"
-    //   y="100"
-    //   text-anchor="middle"
-    //   fill="white"
-    //   font-size="80"
-    //   font-family="Clash Grotesk"
-    //   alignment-baseline="middle">@zkenk has answered the call</text>
-    // </svg>`;
-
     const svg = await generateTwitterCardSVG(blob);
+    const svgBuffer = Buffer.from(svg, 'utf-8');
+
+    // Convert SVG to PNG
+    const width = 1200;
+    const height = 600;
+
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+
+    const img = await loadImage(`data:image/svg+xml;base64,${svgBuffer.toString('base64')}`);
+    ctx.drawImage(img, 0, 0, width, height);
+
+    const imgBuffer = canvas.toBuffer('image/png');
 
 
-    return new Response(svg, {
+    // const pngUrl = URL.createObjectURL(imgBuffer);
+
+
+
+    // Return the PNG image
+    return new Response(imgBuffer, {
       headers: {
-        'Content-Type': 'image/svg+xml'
+        'Content-Type': 'image/png',
       }
     });
   } catch (e) {
     console.error('Failed to fetch image:', e);
-    return error(500, 'Internal Server Error');
+    return new Response('Internal Server Error', { status: 500 });
   }
 }
