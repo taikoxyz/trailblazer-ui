f3668250143d78d33eb60a19ba816546a74267a3
Kenk <kenghin_lim@hotmail.com>
2024-05-18 22:44:16 +0700

    update meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,16 @@
 </script>
 
 <svelte:head>
-  <title>Taiko Trailblazer</title>
+  <title>Taiko Trailblazers: Call of Taiko</title>
+  <meta property="og:image" content="/call-of-taiko.webp" />
+  <meta property="og:title" content="Taiko Trailblazers: Call of Taiko" />
+  <meta property="og:description" content="Will you answer the call?" />
+  <meta property="og:url" content="https://trailblazers.taiko.xyz" />
+  <meta property="og:type" content="website" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Taiko Trailblazers: Call of Taiko" />
+  <meta name="twitter:description" content="Will you answer the call?" />
 </svelte:head>
 
 <!-- spacer -->
