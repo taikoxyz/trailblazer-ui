6c8ab32086a497dc72847133590d5c90a87d42f4
Kenk <kenghin_lim@hotmail.com>
2024-05-18 23:28:50 +0700

    fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 // When setting this to false your entire app will only be rendered on the client,
 // which essentially means you turn your app into an SPA.
 // See https://kit.svelte.dev/docs/page-options#ssr
-export const ssr = false;
-export const prerender = false;
+export const ssr = true;
+export const prerender = true;
 
