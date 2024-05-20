2538dcbef920642513db988dbb82e16d6d18e96b
Kenk <kenghin_lim@hotmail.com>
2024-05-20 20:31:44 +0700

    Try (#36)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
   kit: {
     // https://kit.svelte.dev/docs/single-page-apps
     adapter: adapter({
-      fallback: 'index.html',
+      maxDuration: 300,
     }),
     alias: {
       '$components/*': './src/components/*',
