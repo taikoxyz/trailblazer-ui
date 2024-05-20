91f8fb48e2aa3d6e098bf05340b5be6da44f149f
Bernat Canal Garceran <bernatcanal@gmail.com>
2024-05-20 20:28:23 +0200

    moved +page.ts file to +layout.ts in order to generate a no-render endpoint (#44)

--- src/routes/api/generate/[slug]/[code]/[ext]/+server.ts
+++ src/routes/api/generate/[slug]/[code]/[ext]/+server.ts
@@ -33,20 +33,20 @@ export async function GET({ params }) {
         .cls-1 {
           fill: #d63b95;
         }
-  
+
         .cls-1,
         .cls-2 {
           stroke-width: 0px;
         }
-  
+
         .cls-2 {
           fill: #d63c95;
         }
-  
+
         .cls-3 {
           opacity: .3;
         }
-  
+
         .cls-4 {
           opacity: .7;
         }
