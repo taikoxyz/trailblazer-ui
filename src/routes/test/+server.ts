6ae06020401aad4f9a998e3224785b16bdc7be9e
ben <bennettlikebeckham@gmail.com>
2024-05-20 23:32:40 +0800

    test endpoint

---
+++ src/routes/test/+server.ts
@@ -0,0 +1,7 @@
+import { error, json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ params }) {
+  const { slug: twitterImageId, code: twitterImageCode, ext } = params;
+  return json({test: 'test'})
+}
