017e6c4ca5784040c9632a76081fa074cede17ed
Kenk <kenghin_lim@hotmail.com>
2024-06-06 16:18:41 +0700

    remove unused imports

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-  // variables
-  import { page } from '$app/stores';
   import ConnectButton from '$components/ConnectButton/ConnectButton.svelte';
   import { LogoWithText, TaikoTrailblazersLogo } from '$components/Logo';
-  import { MobileNavigation } from '$components/MobileNavigation';
-  import { ThemeButton } from '$components/ThemeButton';
-  import { NavigationData } from '$configs/navigation';
   import HamburgerIcon from '$images/hamburger.svg';
-  import TrailblazerTag from '$images/trailblazer-tag.svg';
-  import TrailblazerLogo from '$images/taiko-trailblazer-logo.svg';
+
   import XIcon from '$images/x.svg';
   import { account } from '$stores/account';
-  import { screen } from '$stores/responsiveness';
 
   let screenSize: number;
   let mobileMenu = false;
