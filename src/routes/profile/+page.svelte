adc6cf8d8a29e5a020a7e479ef8d531b1d96a405
bearni95 <bernatcanal@gmail.com>
2024-06-07 11:32:54 +0200

    fixed profile redirect

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { account } from "$stores/account";
+  import { onMount } from "svelte";
+
+    import { web3modal } from "$libs/connect";
+    console.log($account)
+
+    async function load(){
+        if ($account && $account.address) {
+            window.location.href=`/profile/${$account.address}`
+            console.log('redirecting', `/profile/${$account.address}`)
+        } else {
+            console.error('no account!')
+            web3modal.open()
+        }
+    }
+
+   $: $account, load()
+</script>
+
