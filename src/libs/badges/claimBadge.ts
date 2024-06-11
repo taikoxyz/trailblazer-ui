c5023c1a5e08f19656f65d63e5fdb7bfb2d21fc4
bearni95 <bernatcanal@gmail.com>
2024-06-11 20:06:23 +0200

    extended debbuging

--- src/libs/badges/claimBadge.ts
+++ src/libs/badges/claimBadge.ts
@@ -3,9 +3,13 @@ import type { Address } from 'viem';
 import type { FACTIONS } from '$configs/badges';
 
 import getMintSignature from './getMintSignature';
+import isSignatureValid from './isSignatureValid';
 import mint from './mint';
 
 export default async function claimBadge(address: Address, factionId: FACTIONS) {
   const { signature } = await getMintSignature(address, factionId);
+  console.info({signature})
+  const isValid = await isSignatureValid(signature, address, factionId);
+  console.info({isValid})
   await mint(address, factionId, signature);
 }
