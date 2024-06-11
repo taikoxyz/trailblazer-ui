de205731897116e3dfbb95bc76fc74785a7caef6
ben <bennettlikebeckham@gmail.com>
2024-06-11 22:21:37 +0800

    fix: leaderboard padding

--- src/components/Table/UserLeaderboard.svelte
+++ src/components/Table/UserLeaderboard.svelte
@@ -11,7 +11,7 @@
   });
 </script>
 
-<div class="overflow-x-auto w-full px-8">
+<div class="overflow-x-auto lg:w-full px-8 mt-[18%] lg:mt-0">
   <div class="flex justify-between mb-4">
     <div class="font-clash-grotesk lg:text-[60px] text-[40px] leading-none lg:leading-relaxed"><span class="text-secondary">User</span> Leaderboard</div>
     <div class="body-small-regular lg:body-regular flex flex-col justify-center">
