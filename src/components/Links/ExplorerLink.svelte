0bbe3cb7b2879acb6566d9112dd5d6544fc7de30
ben <bennettlikebeckham@gmail.com>
2024-06-11 18:56:39 +0800

    fix: add logo

--- src/components/Links/ExplorerLink.svelte
+++ src/components/Links/ExplorerLink.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Icon } from '$components/Icon';
   import { shortenAddress } from '$libs/util/shortenAddress';
 
   export let address: string;
@@ -6,4 +7,4 @@
   let url = `https://taikoscan.io/address/${address}`;
 </script>
 
-<a class="underline body-bold" target="_blank" href={url}>{shorten ? shortenAddress(address) : address}</a>
+<a class="underline body-bold flex align-center" target="_blank" href={url}>{shorten ? shortenAddress(address) : address}<Icon size={10} type='arrow-top-right'/></a>
