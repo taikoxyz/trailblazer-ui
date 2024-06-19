c14c5cf86af3fd7b3f5d7b067e3fc873bd25d2ae
bearni95 <bernatcanal@gmail.com>
2024-06-19 14:58:25 +0200

    moved mock sign hash method onto it's own file

--- src/libs/badges/getMintSignature.ts
+++ src/libs/badges/getMintSignature.ts
@@ -1,5 +1,3 @@
-import { ecsign } from '@ethereumjs/util';
-import { concatSig } from '@metamask/eth-sig-util';
 import { readContract, signMessage } from '@wagmi/core';
 import { type Address } from 'viem';
 
@@ -7,9 +5,10 @@ import type { FACTIONS } from '$configs/badges';
 import { PUBLIC_ENV } from '$env/static/public';
 import { web3modal } from '$libs/connect';
 import { wagmiConfig } from '$libs/wagmi';
-import type { IChainId, IContractData, IToDo } from '$types';
+import type { IChainId, IContractData } from '$types';
 
 import { trailblazersBadgesAbi, trailblazersBadgesAddress } from '../../generated/abi';
+import { mockSignHash } from './getMockMintSignature';
 
 async function signHash(
   config: typeof wagmiConfig,
@@ -46,16 +45,6 @@ async function signHash(
   } catch (error) {
     throw new Error(`Error in signHash: ${error}`);
   }
-}
-// this is a dev-only method
-// it used hardhat account #0 for signing
-// signatures are not valid on mainnet deployment
-export async function mockSignHash(hash: string): Promise<IContractData> {
-  const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-
-  const sig = ecsign(Buffer.from(hash.slice(2), 'hex'), Buffer.from(privateKey.slice(2), 'hex')) as IToDo;
-  const serialized = concatSig(Buffer.from(sig.v.toString(16), 'hex'), sig.r, sig.s);
-  return serialized as IContractData;
 }
 
 export default async function getMintSignature(
