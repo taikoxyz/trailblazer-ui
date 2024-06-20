cf22cccf5159fc0ac7731ecbd1f6630706eee365
Kenk <kenghin_lim@hotmail.com>
2024-06-20 17:50:49 +0800

    feat: unhide ongoing trails

--- src/components/Startpage/Trailblazer.svelte
+++ src/components/Startpage/Trailblazer.svelte
@@ -3,6 +3,7 @@
   import ExploreEcosystem from './ExploreEcosystem.svelte';
   import Factions from './Factions/Factions.svelte';
   import { TrailblazerGuide } from './Guide';
+  import TrailblazerOngoingTrails from './OngoingTrails/TrailblazerOngoingTrails.svelte';
   import { RankUp } from './RankUp';
   import TrailblazerMain from './TrailblazerMain.svelte';
 </script>
@@ -10,10 +11,9 @@
 <div class="f-center container flex-col w-full overflow-visible gap-[100px] mb-[130px]">
   <TrailblazerMain />
   <TrailblazerGuide />
-  <!-- <TrailblazerOngoingTrails /> -->
   <Factions />
-
   <DividerElement />
+  <TrailblazerOngoingTrails />
   <RankUp />
   <ExploreEcosystem />
 </div>
