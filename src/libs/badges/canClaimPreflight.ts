9f38d63a54f6ce190663c9b51ea6e3bc604af22c
bearni95 <bernatcanal@gmail.com>
2024-06-20 14:21:35 +0200

    preflight claim endpoint env value

--- src/libs/badges/canClaimPreflight.ts
+++ src/libs/badges/canClaimPreflight.ts
@@ -1,5 +1,6 @@
 import type { Address } from 'viem';
 
+import { PUBLIC_PREFLIGHT_CLAIM_ENDPOINT } from '$env/static/public';
 import { isDevelopmentEnv } from '$libs/util/isDevelopmentEnv';
 
 export default async function canClaimPreflight(address: Address, badgeId: number): Promise<boolean> {
@@ -7,7 +8,7 @@ export default async function canClaimPreflight(address: Address, badgeId: numbe
     return true;
   }
   try {
-    const baseUrl = 'https://qa.trailblazer.taiko.xyz/whitelist?address=';
+    const baseUrl = `${PUBLIC_PREFLIGHT_CLAIM_ENDPOINT}?address=`;
     const res = await fetch(`${baseUrl}${address}&badgeId=${badgeId}`);
     const json = await res.json();
 
