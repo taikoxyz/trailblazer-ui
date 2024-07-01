41d0fb6f6efe259ab729c386c7f2757b962d0b91
Kenk <kenghin_lim@hotmail.com>
2024-07-01 16:08:17 +0800

    feat: add leaderboard position

--- src/components/Leaderboards/DappsLeaderboard.svelte
+++ src/components/Leaderboards/DappsLeaderboard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Icon } from '$components/Icon';
   import ExplorerLink from '$components/Links/ExplorerLink.svelte';
   import { Skeleton } from '$components/Mock';
   import Paginator from '$components/Paginator/Paginator.svelte';
@@ -19,7 +20,7 @@
   $: totalItems = pageInfo?.total || 0;
   // $: currentPage = pageInfo?.page || 1;
 
-  let headers = ['Dapp', 'Address', 'Points'];
+  let headers = ['No.', 'Dapp', 'Address', 'Points'];
 
   interface MappingValue {
     name: string;
@@ -54,66 +55,77 @@
   });
 </script>
 
-<div class="overflow-x-auto lg:w-full px-8 mt-[18%] lg:mt-0 space-y-[60px]">
+<div class="overflow-x-auto lg:w-full px-5 lg:px-8 mt-[18%] lg:mt-0 space-y-[60px]">
   <DappsHeader />
 
-  <table class="table-lg w-full body-regular text-white rounded-3xl" style="background: rgba(25, 30, 40, .50)">
-    <!-- head -->
-    <thead>
-      <tr>
-        {#each headers as header}
-          <th class="body-regular text-secondary-content text-start pt-8 lg:px-10">{header}</th>
-        {/each}
-      </tr>
-    </thead>
-    <tbody class="rounded-lg">
-      {#each itemsToDisplay as entry, index}
-        {@const hasName = detailMapping[entry.address] && detailMapping[entry.address].name}
-        {@const hasHandle = detailMapping[entry.address] && detailMapping[entry.address].handle}
-        {@const hasIcon = detailMapping[entry.address] && detailMapping[entry.address].icon}
+  <div class="overflow-x-auto rounded-3xl">
+    <table class="table-lg w-full body-regular text-white" style="background: rgba(25, 30, 40, .50)">
+      <!-- head -->
+      <thead>
+        <tr>
+          {#each headers as header}
+            <th class="body-regular text-secondary-content text-start pt-8 lg:px-10">{header}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody class="rounded-lg">
+        {#each itemsToDisplay as entry, index}
+          {@const rank = index + 1}
+          {@const fillClass =
+            rank === 1 ? 'fill-[#EBB222]' : rank === 2 ? 'fill-[#91969F]' : rank === 3 ? 'fill-[#775602]' : ''}
+          {@const hasName = detailMapping[entry.address] && detailMapping[entry.address].name}
+          {@const hasHandle = detailMapping[entry.address] && detailMapping[entry.address].handle}
+          {@const hasIcon = detailMapping[entry.address] && detailMapping[entry.address].icon}
 
-        <tr id={index.toString()} class="row h-12">
-          <td class="lg:px-10">
-            <div class="flex gap-[20px] align-center">
-              {#if hasIcon}
-                <div class="avatar">
-                  <div class="w-12 rounded-full">
-                    <img alt="icon" src="/{detailMapping[entry.address].icon}" />
-                  </div>
-                </div>
-              {:else}
-                <Skeleton
-                  class="hidden lg:table-cell"
-                  width="w-12"
-                  height="h-12"
-                  bgColor="bg-pink-200"
-                  shineColor="bg-pink-100" />
-              {/if}
-              <div class="flex flex-col justify-around">
-                {#if hasName}
-                  <div class="body-bold">{detailMapping[entry.address].name}</div>
-                {:else}
-                  <div class="body-bold">tbd</div>
-                {/if}
-                {#if hasHandle}
-                  <div class="body-small-regular">{detailMapping[entry.address].handle}</div>
-                {/if}
+          <tr id={index.toString()} class="row h-12 hover:bg-neutral-background">
+            <td class=" h-full table-cell body-bold">
+              <div class="f-center gap-[12px]">
+                <Icon type="trophy" {fillClass} />
+                {rank}
               </div>
-            </div>
-          </td>
-          <td class="lg:table-cell hidden lg:px-10 body-regular"><ExplorerLink address={entry.address} /></td>
-          <td class="text-start lg:hidden table-cell lg:px-10 body-regular"
-            ><ExplorerLink address={entry.address} shorten={true} /></td>
-          <td class="lg:px-10 body-regular">{formatNumbers(Math.round(entry.score))}</td>
-        </tr>
-      {/each}
-      {#if itemsToDisplay.length === 0}
-        <tr class="row h-12">
-          <td class="lg:px-10" colspan="3">No data available</td>
-        </tr>
-      {/if}
-    </tbody>
-  </table>
+            </td>
+            <td class="lg:px-10">
+              <div class="flex gap-[20px] align-center">
+                {#if hasIcon}
+                  <div class="avatar">
+                    <div class="w-12 rounded-full">
+                      <img alt="icon" src="/{detailMapping[entry.address].icon}" />
+                    </div>
+                  </div>
+                {:else}
+                  <Skeleton
+                    class="hidden lg:table-cell"
+                    width="w-12"
+                    height="h-12"
+                    bgColor="bg-pink-200"
+                    shineColor="bg-pink-100" />
+                {/if}
+                <div class="flex flex-col justify-around">
+                  {#if hasName}
+                    <div class="body-bold">{detailMapping[entry.address].name}</div>
+                  {:else}
+                    <div class="body-bold">tbd</div>
+                  {/if}
+                  {#if hasHandle}
+                    <div class="body-small-regular">{detailMapping[entry.address].handle}</div>
+                  {/if}
+                </div>
+              </div>
+            </td>
+            <td class="lg:table-cell hidden lg:px-10 body-regular"><ExplorerLink address={entry.address} /></td>
+            <td class="text-start lg:hidden table-cell lg:px-10 body-regular"
+              ><ExplorerLink address={entry.address} shorten={true} /></td>
+            <td class="lg:px-10 body-regular">{formatNumbers(Math.round(entry.score))}</td>
+          </tr>
+        {/each}
+        {#if itemsToDisplay.length === 0}
+          <tr class="row h-12">
+            <td class="lg:px-10" colspan="3">No data available</td>
+          </tr>
+        {/if}
+      </tbody>
+    </table>
+  </div>
 
   <Paginator
     {pageSize}
