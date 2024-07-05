67bf42c8ca7a4fddbcee328b6e920fb28511c030
Korbinian <korbiniank@outlook.de>
2024-07-05 17:05:25 +0200

    refactor carousels, add defi carousel

---
+++ src/components/Partner/GamingPartners/GamingPartners.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { PartnersTemplate } from '$components/Partner/';
+
+  import { gamingCarouselItems } from './gamingCarouselItems';
+</script>
+
+<PartnersTemplate
+  title="Our gaming partners"
+  subtitle="Play a game, <span class='text-base-content'>earn points, and climb the ranks</span>"
+  carouselItems={gamingCarouselItems} />
