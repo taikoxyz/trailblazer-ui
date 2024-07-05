e9c5dd464b5e082595751768e22d75e38ded6298
bearni95 <bernatcanal@gmail.com>
2024-07-05 10:58:58 +0200

    removed legacy code

--- src/libs/badges/mint.ts
+++ src/libs/badges/mint.ts
@@ -1,4 +1,4 @@
-import { getTransaction, writeContract } from '@wagmi/core';
+import { writeContract } from '@wagmi/core';
 import { type Address } from 'viem';
 
 import { FACTIONS } from '$configs/badges';
@@ -8,7 +8,6 @@ import { pendingTransactions } from '$stores/pendingTransactions';
 import type { IContractData } from '$types';
 
 import { trailblazersBadgesAbi, trailblazersBadgesAddress } from '../../generated/abi';
-import { canMint } from './canMint';
 import isSignatureValid from './isSignatureValid';
 
 export default async function mint(address: Address, factionId: FACTIONS, signature: IContractData): Promise<string> {
@@ -17,26 +16,6 @@ export default async function mint(address: Address, factionId: FACTIONS, signat
 
   if (!signatureValid) {
     throw new Error('Invalid signature');
-  }
-  const previousTxStorageKey = 'trailblazersBadgesMintTx';
-  const previousTxHash = localStorage.getItem(previousTxStorageKey);
-
-  if (previousTxHash) {
-    const { blockHash } = await getTransaction(wagmiConfig, {
-      hash: previousTxHash as Address,
-    });
-
-    if (!blockHash) {
-      throw new Error('Previous claim transaction is still pending');
-    } else {
-      localStorage.removeItem(previousTxStorageKey);
-
-      // if not minting anymore, resolve!
-      const isMinting = await canMint(signature, address, factionId);
-      if (!isMinting) {
-        return previousTxHash;
-      }
-    }
   }
 
   const tx = await writeContract(wagmiConfig, {
