2a95f6aba31a128bc5cee7d7301b27295a78af21
bearni95 <bernatcanal@gmail.com>
2024-07-15 15:40:45 +0200

    simplified gas calculations for pre-flight

--- src/libs/badges/gasCheckPreflight.ts
+++ src/libs/badges/gasCheckPreflight.ts
@@ -1,47 +1,19 @@
-import { getTransactionReceipt } from '@wagmi/core';
+import { getGasPrice } from '@wagmi/core';
 import { getBalance } from '@wagmi/core';
-import { type Address, createPublicClient, http } from 'viem';
-import { taiko, taikoHekla } from 'viem/chains';
+import { type Address } from 'viem';
 
-import { chainId } from '$libs/chain';
-import { isDevelopmentEnv } from '$libs/util/isDevelopmentEnv';
 import { wagmiConfig } from '$libs/wagmi';
 
-import { trailblazersBadgesAbi, trailblazersBadgesAddress } from '../../generated/abi';
-
 export default async function gasCheckPreflight(address: Address): Promise<boolean> {
-  const publicClient = createPublicClient({
-    chain: isDevelopmentEnv ? taikoHekla : taiko,
-    transport: http(isDevelopmentEnv ? 'https://rpc.hekla.taiko.xyz' : 'https://rpc.mainnet.taiko.xyz'),
-  });
-
-  const currentBlockNumber = await publicClient.getBlockNumber();
-
-  const logs = await publicClient.getContractEvents({
-    address: trailblazersBadgesAddress[chainId],
-    abi: trailblazersBadgesAbi,
-    eventName: 'BadgeCreated',
-    fromBlock: currentBlockNumber - 1000n,
-  });
-
   const balance = await getBalance(wagmiConfig, {
     address,
   });
 
   const balanceGwei = balance.value;
+  // max gas used by 15/07/2024 = 239547
+  const maxGasUsed = 240_000n;
+  const gasPrice = await getGasPrice(wagmiConfig);
+  const price = maxGasUsed * gasPrice;
 
-  const txPrices: bigint[] = [];
-  for (const log of logs) {
-    const receipt = await getTransactionReceipt(wagmiConfig, { hash: log.transactionHash });
-
-    const { effectiveGasPrice, gasUsed } = receipt;
-
-    const price = gasUsed * effectiveGasPrice;
-
-    txPrices.push(price);
-  }
-
-  const avgTxPrice = txPrices.reduce((a, b) => a + b, 0n) / BigInt(txPrices.length);
-
-  return balanceGwei > avgTxPrice;
+  return balanceGwei > price;
 }
