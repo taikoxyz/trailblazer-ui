24c6792b7a11cf1584facd64b913d85a48cb9cb4
Korbinian <korbiniank@outlook.de>
2024-07-17 13:13:31 +0200

    rm

--- src/libs/util/isDevelopmentEnv.ts
+++ src/libs/util/isDevelopmentEnv.ts
@@ -1,3 +1,3 @@
-import { PUBLIC_ENV } from '$env/static/public';
+const { MODE } = import.meta.env;
 
-export const isDevelopmentEnv = PUBLIC_ENV === 'development';
+export const isDevelopmentEnv = MODE === 'development';
