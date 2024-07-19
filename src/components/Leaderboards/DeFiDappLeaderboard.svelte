fb33343fcf69e5c1401aa89679454497ec842298
Kenk <kenghin_lim@hotmail.com>
2024-07-19 09:37:49 +0200

    use i18n

--- src/components/Leaderboards/DeFiDappLeaderboard.svelte
+++ src/components/Leaderboards/DeFiDappLeaderboard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { t } from 'svelte-i18n';
   import { Icon } from '$components/Icon';
   import { Skeleton } from '$components/Mock';
   import { formatNumbers } from '$libs/util/formatNumbers';
@@ -17,41 +18,40 @@
   <div class="f-center flex-col">
     <div class="flex gap-5">
       <img src="/leaderboard/defi/left-laser.svg" alt="left laser" class="hidden lg:block" />
-      <div class="display-medium-medium text-center pb-[30px]">Join the DeFi competition starting July 15th!</div>
+      <div class="display-medium-medium text-center pb-[30px]">{$t('leaderboard.defi.banner.title')}</div>
       <img src="/leaderboard/defi/right-laser.svg" alt="right laser" class="hidden lg:block" />
     </div>
     <div class="text-secondary-content body-regular text-center max-w-[485px] pb-[80px]">
-      Compete for a month-long jackpot by increasing your TVL on DeFiLlama. Rewards will be based on performance
-      throughout the competition, ending August 9th.
+      {$t('leaderboard.defi.banner.description')}
     </div>
 
     <div
       class="flex flex-col sm:flex-col lg:flex-row relative border-2 border-primary-border-hover rounded-3xl px-[90px] lg:px-[46px] pt-[90px] lg:pt-[58px] pb-[51px] gap-[30px] lg:gap-[83px]">
       <div
         class="f-col lg:block absolute border-2 body-bold border-primary-border-hover top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-5 py-4 min-w-[209px] rounded-full text-center bg-primary-background">
-        <span> Prize Pool</span>
-        <span class="text-primary-brand">175K TAIKO Tokens</span>
+        <span> {$t('leaderboard.defi.prize.1')}</span>
+        <span class="text-primary-brand">{$t('leaderboard.defi.prize.2')}</span>
       </div>
 
       <div class="f-center gap-4 w-fit">
         <img src="/leaderboard/defi/first.svg" alt="100K TAIKO Tokens" class="size-[48px]" />
         <div class="flex flex-col">
-          <span class="title-section-bold">100K</span>
-          <span class="body-regular">TAIKO Tokens</span>
+          <span class="title-section-bold">{$t('leaderboard.defi.prize_breakdown.first.title')}</span>
+          <span class="body-regular">{$t('leaderboard.defi.prize_breakdown.first.prize')}</span>
         </div>
       </div>
       <div class="f-center gap-4 w-fit">
         <img src="/leaderboard/defi/second.svg" alt="50K TAIKO Tokens" class="size-[48px]" />
         <div class="flex flex-col">
-          <span class="title-section-bold">50K</span>
-          <span class="body-regular">TAIKO Tokens</span>
+          <span class="title-section-bold">{$t('leaderboard.defi.prize_breakdown.second.title')}</span>
+          <span class="body-regular">{$t('leaderboard.defi.prize_breakdown.second.prize')}</span>
         </div>
       </div>
       <div class="f-center gap-4 w-fit">
         <img src="/leaderboard/defi/third.svg" alt="25K TAIKO Tokens" class="size-[48px]" />
         <div class="flex flex-col">
-          <span class="title-section-bold">25K</span>
-          <span class="body-regular">TAIKO Tokens</span>
+          <span class="title-section-bold">{$t('leaderboard.defi.prize_breakdown.third.title')}</span>
+          <span class="body-regular">{$t('leaderboard.defi.prize_breakdown.third.prize')}</span>
         </div>
       </div>
     </div>
