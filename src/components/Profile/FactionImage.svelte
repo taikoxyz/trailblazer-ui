873d9db99e9e4372fcb0e30b0af7c3e3d01b4ce0
bearni95 <bernatcanal@gmail.com>
2024-07-31 12:16:22 +0200

    even more simple faction image assignation

--- src/components/Profile/FactionImage.svelte
+++ src/components/Profile/FactionImage.svelte
@@ -6,24 +6,18 @@
   export let unlocked: boolean = false;
   export let movement: Movements = 0;
 
-  $: dirName = `/factions/${type.toLowerCase()}`;
-  $: fileName = MovementNames[movement].toLowerCase();
-  $: posterUrl = `${dirName}/${fileName}.png`;
-  $: mp4Url = `${dirName}/${fileName}.mp4`;
-  $: webmUrl = `${dirName}/${fileName}.webm`;
+  $: baseUrl = `/factions/${type.toLowerCase()}/${MovementNames[movement].toLowerCase()}`;
 </script>
 
 <div class="relative w-full h-full z-0">
   <video
-    poster={posterUrl}
+    poster="{baseUrl}.png"
     loop
     autoplay={unlocked}
     class="pointer-events-none rounded-[20px] absolute left-0 top-0 z-20">
     <track kind="captions" />
-
-    <source src={webmUrl} type="video/webm" />
-
-    <source src={mp4Url} type="video/mp4" />
+    <source src="{baseUrl}.webm" type="video/webm" />
+    <source src="{baseUrl}.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
