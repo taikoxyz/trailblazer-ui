9711d46ebc5ef37fbb8f8c0a4c61c7b70a7a6eff
Kenk <kenghin_lim@hotmail.com>
2024-08-07 11:03:53 +0700

    update env var

--- src/libs/graphql/client.ts
+++ src/libs/graphql/client.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
 export const graphqlClient = new ApolloClient({
-  uri: 'https://api.goldsky.com/api/public/project_clzgosmls42bq01wi0shn6fpi/subgraphs/tbz/2.1.10/gn',
+  uri:
+    process.env.PUBLIC_TRAILBLAZER_GRAPHQL_URL ||
+    'https://api.goldsky.com/api/public/project_clzgosmls42bq01wi0shn6fpi/subgraphs/tbz/2.1.10/graphql',
   cache: new InMemoryCache(),
 });
