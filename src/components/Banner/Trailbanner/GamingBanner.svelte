afd290811d1637c676477e8515ae1c34a7819e27
ben <bennettlikebeckham@gmail.com>
2024-08-08 15:29:10 +0800

    feat: gaming banner carousel

---
+++ src/components/Banner/Trailbanner/GamingBanner.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { t } from 'svelte-i18n';
+
+  import AbstractBannerDouble from './AbstractBannerDouble.template.svelte';
+</script>
+
+<AbstractBannerDouble
+  trailNumber="7"
+  title={$t('trailbanner.gaming.title')}
+  description={$t('trailbanner.gaming.description')}
+  href="https://taiko.mirror.xyz/BRPO4OzcnPanyhyfa80yzvpidt8etBS2QwNdP0TVk8o"
+  buttonText={$t('common.learn_more')}
+  images={{
+    content: {
+      xl: '/trailbanner/gaming/xl/icons.png',
+      lg: '/trailbanner/gaming/lg/icons.png',
+      md: '/trailbanner/gaming/md/icons.png',
+      sm: '/trailbanner/gaming/sm/icons.png',
+    },
+    background: {
+      xl: '/trailbanner/common/xl/bg.svg',
+      lg: '/trailbanner/common/lg/bg.svg',
+      md: '/trailbanner/common/md/bg.svg',
+      sm: '/trailbanner/common/sm/bg.svg',
+    },
+  }} />
--- src/components/Banner/Trailbanner/index.ts
+++ src/components/Banner/Trailbanner/index.ts
@@ -1,3 +1,4 @@
 export { default as DefiBanner } from './DefiBanner.svelte';
 export { default as DomainBanner } from './DomainBanner.svelte';
 export { default as NFTBanner } from './NFTBanner.svelte';
+export { default as GamingBanner } from './GamingBanner.svelte';
