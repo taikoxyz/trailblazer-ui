ea365d0efcffc31eae5c9e085850d7780c3300e6
Korbinian <korbiniank@outlook.de>
2024-08-13 22:52:10 +0200

    Squashed commit of the following:
    commit 684149cda586487dfc47d18581c30e3547c74d35
    Merge: 34ad9b2 c10feca
    Author: Korbinian <KorbinianK@users.noreply.github.com>
    Date:   Tue Aug 13 10:52:51 2024 +0200
    
        change order of games (#203)
    
    commit c10feca39238961b488763823c54950de65779bd
    Author: Korbinian <korbiniank@outlook.de>
    Date:   Tue Aug 13 10:50:33 2024 +0200
    
        change order

--- src/components/Partner/GamingPartners/gamingCarouselItems.ts
+++ src/components/Partner/GamingPartners/gamingCarouselItems.ts
@@ -3,91 +3,91 @@ import type { CarouselItemType } from '$components/Carousel/types';
 export const gamingCarouselItems: CarouselItemType[] = [
   {
     id: '1',
-    title: 'Crack & Stack',
-    description:
-      'Top-down multiplayer game. Enter the Ethereum mines, set traps, mine ores, escape the goblins and escape with your loot.',
-    image: '/dapps/gaming/crackandstack.svg',
-    link: 'https://crackandstack.com/',
-    cta: 'Play Now',
-  },
-  {
-    id: '2',
-    title: 'Looper Lands',
-    description:
-      'LooperLands is a free-to-play community-pixelverse. Traverse between captivating creator made worlds, each with its own complete stories and unique minigames.',
-    image: '/dapps/gaming/looperlands.svg',
-    link: 'https://looperlands.io/',
-    cta: 'Play Now',
-  },
-  {
-    id: '3',
-    title: 'Ulti-Pilot',
-    description:
-      'Explore Ultiverse with Ulti-Pilot, powered by Bodhi AI. Create agents, explore daily, and earn rewards. Free access now.',
-    image: '/dapps/gaming/ultiverse.png',
-    link: 'https://pilot.ultiverse.io/',
-    cta: 'Play Now',
-  },
-  {
-    id: '4',
-    title: 'Zypher z2048',
-    description:
-      'Immerse in z2048, a strategic on-chain game blending classic 2048 with fair, scalable gameplay via Zypher’s UZKGE.',
-    image: '/dapps/gaming/zyphergames.png',
-    link: 'https://zypher.game/2048/',
-    cta: 'Play Now',
-  },
-  {
-    id: '5',
-    title: '0xAstra',
-    description:
-      '0xAstra is an onchain SLG game, where you harness cross-chain resources with bridge technology to build and defend your cosmic homeland. Strategize, expand, and dominate in this interstellar conquest.',
-    image: '/dapps/gaming/0xastra.svg',
-    link: 'https://0xastra.xyz/',
-    cta: 'Play Now',
-  },
-  {
-    id: '6',
     title: 'EVM Warfare',
     description:
       'Fight, Earn, Dominate. Thrilling third-person shooter powered by Unreal Engine 5. Dominate deathmatches, claim instant rewards based on performance.',
-    image: '/dapps/gaming/evmwarfare.png',
+    image: '/games/evmwarfare.png',
     link: 'https://evmwarfare.com/',
     cta: 'Play Now',
   },
   {
-    id: '7',
+    id: '2',
     title: 'Robots.Farm',
     description:
       'Robot.farms is a gamified trading platform that seamlessly merges decentralized finance with a captivating gaming experience.',
-    image: '/dapps/gaming/robotsfarm.jpg',
+    image: '/games/robotsfarm.jpg',
     link: 'https://robots.farm/',
     cta: 'Play Now',
   },
   {
-    id: '8',
+    id: '3',
     title: 'Bullish',
     description: 'Bullish is the first seamless transaction tech in Omni Layer2 cross-rollup game.',
-    image: '/dapps/gaming/bullish.png',
+    image: '/games/bullish.png',
     link: 'https://bullishs.io/',
     cta: 'Play Now',
   },
   {
-    id: '9',
+    id: '4',
     title: 'Brigade',
     description:
       'Brigade is a play-to-earn game that you can start completely free. It is the perfect opportunity to immerse yourself in the Taco Studios world.',
-    image: '/dapps/gaming/brigade.png',
+    image: '/games/brigade.png',
     link: 'https://brigadegame.io/',
     cta: 'Play Now',
   },
   {
-    id: '10',
+    id: '5',
     title: 'World of Dypians',
     description:
       'Experience unique gameplay and explore a world without end in a quest to form your destiny. The opportunities are endless ensuring infinite playability.',
-    image: '/dapps/gaming/dypians.png',
+    image: '/games/dypians.png',
     link: 'https://www.worldofdypians.com/',
+    cta: 'Play Now',
+  },
+  {
+    id: '6',
+    title: 'Crack & Stack',
+    description:
+      'Top-down multiplayer game. Enter the Ethereum mines, set traps, mine ores, escape the goblins and escape with your loot.',
+    image: '/games/crackandstack.svg',
+    link: 'https://crackandstack.com/',
+    cta: 'Play Now',
+  },
+  {
+    id: '7',
+    title: 'Looper Lands',
+    description:
+      'LooperLands is a free-to-play community-pixelverse. Traverse between captivating creator made worlds, each with its own complete stories and unique minigames.',
+    image: '/games/looperlands.svg',
+    link: 'https://looperlands.io/',
+    cta: 'Play Now',
+  },
+  {
+    id: '8',
+    title: 'Ulti-Pilot',
+    description:
+      'Explore Ultiverse with Ulti-Pilot, powered by Bodhi AI. Create agents, explore daily, and earn rewards. Free access now.',
+    image: '/games/ultiverse.png',
+    link: 'https://pilot.ultiverse.io/',
+    cta: 'Play Now',
+  },
+  {
+    id: '9',
+    title: 'Zypher z2048',
+    description:
+      'Immerse in z2048, a strategic on-chain game blending classic 2048 with fair, scalable gameplay via Zypher’s UZKGE.',
+    image: '/games/zyphergames.png',
+    link: 'https://zypher.game/2048/',
+    cta: 'Play Now',
+  },
+  {
+    id: '10',
+    title: '0xAstra',
+    description:
+      '0xAstra is an onchain SLG game, where you harness cross-chain resources with bridge technology to build and defend your cosmic homeland. Strategize, expand, and dominate in this interstellar conquest.',
+    image: '/games/0xastra.svg',
+    link: 'https://0xastra.xyz/',
     cta: 'Play Now',
   },
 ];
