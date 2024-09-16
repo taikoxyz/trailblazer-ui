67de660d2426c1b3bbfc7772e439adc77a994db5
ben <bennettlikebeckham@gmail.com>
2024-09-16 19:11:47 +0800

    refactor: styles

--- src/components/Startpage/Factions/Factions.svelte
+++ src/components/Startpage/Factions/Factions.svelte
@@ -6,41 +6,95 @@
   import { FactionsGallery } from '$components/FactionsGallery';
   import Icon from '$components/Icon/Icon.svelte';
   import Title from '$components/Title/Title.svelte';
+  import { classNames } from '$libs/util/classNames';
 
   import { DividerElement } from '../DividerElement';
+
+  let carouselRef: InstanceType<typeof FactionsGallery>;
 
   const handleFactionsButton = () => {
     goto('/about');
   };
-  let carouselRef: InstanceType<typeof FactionsGallery>;
 
   function scrollLeft() {
-    carouselRef.scrollLeft(); // Accessing the function on the child
+    carouselRef.scrollLeft();
   }
 
   function scrollRight() {
-    carouselRef.scrollRight(); // Accessing the function on the child
+    carouselRef.scrollRight();
   }
+
+  const containerClasses = classNames(
+    'flex',
+    'flex-col',
+    'w-screen',
+    'bg-base-200',
+    'rounded-t-[30px]',
+    'rounded-b-none',
+  );
+
+  const sectionClasses = classNames('flex', 'flex-col', 'w-screen', 'lg:h-[413px]', 'justify-center');
+
+  const contentClasses = classNames('f-center', 'flex-col', 'lg:flex-row', 'gap-[100px]', 'px-8');
+
+  const titleContainerClasses = classNames(
+    'lg:w-[562px]',
+    'md:w-[402px]',
+    'md:pt-10',
+    'flex',
+    'flex-col',
+    'justify-between',
+  );
+
+  const subtitleClasses = classNames(
+    'w-full',
+    'xl:text-left',
+    'mb-[40px]',
+    'md:pt-10',
+    'md:w-[189px]',
+    'pt-8',
+    'w-[100px]',
+  );
+
+  const buttonContainerClasses = classNames(
+    'md:m-auto',
+    'lg:m-0',
+    'flex',
+    'gap-4',
+    'md:w-[402px]',
+    'md:justify-start',
+    'lg:justify-center',
+    'md:mb-2',
+    'justify-center',
+    'xl:justify-end',
+    'h-full',
+    'bottom-0',
+    'items-end',
+    'self-start',
+  );
+
+  const scrollButtonClasses = classNames('f-center', 'btn-circle', 'border', 'border-primary-brand');
+
+  const scrollRightButtonClasses = classNames(
+    scrollButtonClasses,
+    'bg-primary-brand',
+    'hover:bg-primary-interactive-hover',
+  );
 </script>
 
-<!-- Taiko Faction -->
-<div class="flex flex-col w-screen bg-base-200 rounded-t-[30px] rounded-b-none">
-  <div class="flex flex-col w-screen lg:h-[413px] justify-center">
-    <div class="f-center flex-col lg:flex-row gap-[100px] px-8">
-      <!-- Title text: Unlock your potential -->
-      <div class="lg:w-[562px] md:w-[402px] md:pt-10 flex flex-col justify-between">
-        <div class="w-full xl:text-left mb-[40px] md:pt-10 md:w-[189px] pt-8 w-[100px]">
+<div class={containerClasses}>
+  <div class={sectionClasses}>
+    <div class={contentClasses}>
+      <div class={titleContainerClasses}>
+        <div class={subtitleClasses}>
           <span class="text-primary-base-content font-clash-grotesk text-[20px]">{$t('factions.subtitle')}</span>
-          <!-- Underline -->
           <div class="w-[60px] h-[3px] bg-primary-brand mt-[10px]"></div>
         </div>
 
-        <!-- Title text: Taiko Factions -->
         <Title class="text-primary !text-[57px]" center="left" width="full">
           Taiko <span class="text-primary-base-content">Factions</span>
         </Title>
 
-        <!-- Sub text: Updated to match the new description styling -->
         <div class="w-full f-col mt-12">
           <div class="body-regular text-primary-base-content text-left text-[16px]">
             {$t('factions.gallery.description')}
@@ -48,15 +102,11 @@
         </div>
       </div>
 
-      <!-- Carousel buttons -->
-      <div
-        class="md:m-auto lg:m-0 flex gap-4 md:w-[402px] md:justify-start lg:justify-center md:mb-2 justify-center xl:justify-end h-full bottom-0 items-end self-start">
-        <button class="f-center btn-circle border border-primary-brand" on:click={scrollLeft}>
+      <div class={buttonContainerClasses}>
+        <button class={scrollButtonClasses} on:click={scrollLeft}>
           <Icon class="-translate-x-[2px]" type="chevron-left" />
         </button>
-        <button
-          class="f-center btn-circle bg-primary-brand border-primary-brand hover:bg-primary-interactive-hover"
-          on:click={scrollRight}>
+        <button class={scrollRightButtonClasses} on:click={scrollRight}>
           <Icon class="translate-x-[2px]" type="chevron-right" />
         </button>
       </div>
@@ -64,26 +114,26 @@
   </div>
 
   <FactionsGallery bind:this={carouselRef} />
-  <div class="flex flex-col w-screen justify-center">
-    <div class="f-center flex-col lg:flex-row gap-[100px] px-8">
-      <!-- Title text: Unlock your potential -->
-      <div class="lg:w-[562px] md:w-[402px] pb-12 flex flex-col justify-between">
+
+  <div class={sectionClasses}>
+    <div class={contentClasses}>
+      <div class={titleContainerClasses}>
         <Title class="my-[40px] !max-w-full !w-full !mb-[16px]" center="left">
-          <span class="text-primary-base-content !text-[45px] !leading-4 md:!text-[57px]"
-            >{$t('factions.earn.title')}</span>
+          <span class="text-primary-base-content !text-[45px] !leading-4 md:!text-[57px]">
+            {$t('factions.earn.title')}
+          </span>
         </Title>
         <div class="xl:text-left text-secondary-content max-w-[482px] mb-[40px] text-[16px]">
           {$t('factions.earn.description')}
         </div>
         <ActionButton priority="primary" class="!w-[210px] w-full" on:click={handleFactionsButton}>
-          {$t('buttons.factions.learn_about')}</ActionButton>
+          {$t('buttons.factions.learn_about')}
+        </ActionButton>
       </div>
-
-      <div
-        class="flex gap-4 md:w-[402px] md:justify-start md:mb-2 justify-center xl:justify-end h-full bottom-0 items-end">
-      </div>
+      <div class={buttonContainerClasses}></div>
     </div>
   </div>
+
   <div class="w-full">
     <DividerElement />
   </div>
