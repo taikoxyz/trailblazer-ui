7c47f5d863d626563deba07501c536412f62ff4e
ben <bennettlikebeckham@gmail.com>
2024-09-17 03:21:43 +0800

    fix: inline

--- src/components/Title/Title.svelte
+++ src/components/Title/Title.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
   import { classNames } from '$libs/util/classNames';
 
-  export let width = classNames('xl:max-w-[434px] md:w-1/2', $$props.class);
-  export let center = classNames('md:text-center', $$props.class);
+  export let width = $$props.class;
+  export let center = $$props.class;
+
+  const widthClasses = classNames('xl:max-w-[434px]', 'md:w-1/2', width);
+  const centerClasses = classNames('md:text-left', center);
+
+  const titleClasses = classNames(
+    widthClasses,
+    centerClasses,
+    'font-clash-grotesk',
+    'text-secondary-brand',
+    'tracking-[-1.5px]',
+    'md:text-[75px]/[70px]',
+    'text-[57px]/[52px]',
+    'leading-12',
+    'md:leading-none',
+  );
 </script>
 
-<div
-  class="{width} font-clash-grotesk text-secondary-brand tracking-[-1.5px] md:text-[75px]/[70px] text-[57px]/[52px] {center} md:text-left leading-12 md:leading-none">
+<div class={titleClasses}>
   <slot />
 </div>
