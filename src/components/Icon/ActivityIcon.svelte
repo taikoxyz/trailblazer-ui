f2ae319e588929e99a998f1ecb48725b2043b27e
Korbinian <korbiniank@outlook.de>
2024-09-27 16:11:36 +0200

    transaction view

--- src/components/Icon/ActivityIcon.svelte
+++ src/components/Icon/ActivityIcon.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+  import { classNames } from '$libs/util/classNames';
+
   import { Icon, type IconType } from '.';
   export let type: IconType = 'double-coin';
+  const classes = classNames(
+    'f-center',
+    'min-w-[24px]',
+    'min-h-[24px]',
+    'bg-primary-brand',
+    'rounded-full',
+    $$props.class,
+  );
 </script>
 
-<div class="f-center size-6 bg-primary-brand rounded-full">
+<div class={classes}>
   <Icon {type} vWidth={16} vHeight={16} class="size-[16px]" fillClass="fill-primary-content" />
 </div>
