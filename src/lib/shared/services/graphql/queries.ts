e0667b4d01cfab51d808b8ea5120d7c1ef204de1
Korbinian <korbiniank@outlook.de>
2024-10-02 01:01:04 +0200

    profile adapter test

--- src/lib/shared/services/graphql/queries.ts
+++ src/lib/shared/services/graphql/queries.ts
@@ -14,3 +14,25 @@ export const USER_NFTS_QUERY = gql`
     }
   }
 `;
+
+export const USER_PROFILE_PICTURE_QUERY = gql`
+  query PfpTokenURI($address: String) {
+    profilePicture(id: $address) {
+      id
+      tokenAddress
+      tokenId
+      tokenURI
+    }
+  }
+`;
+
+export const USER_PROFILE_PICTURES_QUERY = gql`
+  query PfpTokenURI($address: String) {
+    profilePictures(id: $address) {
+      id
+      tokenAddress
+      tokenId
+      tokenURI
+    }
+  }
+`;
