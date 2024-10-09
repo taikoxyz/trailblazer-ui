b677f097aec75d0a884e9b25dfaf31443de72082
Korbinian <korbiniank@outlook.de>
2024-10-10 01:30:32 +0200

    banner carousel, s1 claim banner

---
+++ src/lib/domains/splashpage/components/Banner/types.ts
@@ -0,0 +1,6 @@
+import type { SvelteComponent } from 'svelte';
+
+export interface Slide {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: typeof SvelteComponent<any>;
+}
