c1e1ae39e148d6185bde03ead98ff39df24cddb3
bearni95 <bernatcanal@gmail.com>
2024-10-09 13:26:57 +0200

    more testing on qa

--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -1,6 +1,10 @@
 import { type Handle } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
-export const handle: Handle = async () => {
-  throw redirect(400, `/error`);
+export const handle: Handle = async ({ event, resolve }) => {
+  if (event.url.pathname !== '/error') {
+    throw redirect(400, `/error`);
+  }
+
+  return resolve(event);
 };
