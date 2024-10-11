3b9b0a2be268f7067caf18352f9ab8cb74dd3706
Korbinian <korbiniank@outlook.de>
2024-10-11 15:04:08 +0200

    s1 points

--- src/lib/domains/profile/types/UserStats.ts
+++ src/lib/domains/profile/types/UserStats.ts
@@ -1,10 +1,24 @@
+export type SeasonHistoryEntry = {
+  score: number;
+  multiplier: number;
+  total: number;
+  // rank: string;
+  // title: string;
+  // level: string;
+  // total?: string;
+  // rankPercentile?: string;
+};
+
+export type UserStatsDetails = {
+  score: number;
+  rank: string;
+  title: string;
+  level: string;
+  total: string;
+  rankPercentile?: string;
+  seasonHistory?: Record<number, SeasonHistoryEntry>;
+};
+
 export type UserStats = {
-  userStats: {
-    score: number;
-    rank: string;
-    title: string;
-    level: string;
-    total: string;
-    rankPercentile?: string;
-  };
+  userStats: UserStatsDetails;
 };
