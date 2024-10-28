e99d401e64a6e22cf5e9160effad697c88b042b9
bearni95 <bernatcanal@gmail.com>
2024-10-28 22:18:50 +0100

    force build

--- src/lib/domains/badges/adapter/BadgeMigrationAdapter._test.ts
+++ src/lib/domains/badges/adapter/BadgeMigrationAdapter._test.ts
@@ -48,7 +48,7 @@ const createMockQueryResult = <T>(data: T): ApolloQueryResult<T> => ({
 describe('BadgeMigrationAdapter', () => {
   let adapter: BadgeMigrationAdapter;
 
-  //const mockTxHash = '0xTransactionHash';
+  // const mockTxHash = '0xTransactionHash';
 
   // const SEASON_1 = 1;
   // const SEASON_2 = 2
