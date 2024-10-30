b456b339df3bd682cb2e66e34aa5ee11d561f30f
swarnabhasinha <swarnabhasinha@gmail.com>
2024-10-30 14:29:45 +0530

    fix

--- src/lib/domains/ecosystem/components/partners.ts
+++ src/lib/domains/ecosystem/components/partners.ts
@@ -303,7 +303,7 @@ const EcosystemPartners: IEcosystemPartner[] = [
     name: 'TakoTako',
     description:
       'TAKOTAKO emphasizes on real yield by offering competitive rates and sharing almost half of its revenue with users.',
-    logo: '/dapps/takotako.svg',
+    logo: '/dapps/takotako.png',
     category: 'DEX',
     url: 'https://takotako.xyz',
   },
