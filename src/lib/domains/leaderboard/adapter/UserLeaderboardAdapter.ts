2a66fcc0c8c94b1644ed4049a3223fb5c92304cf
Korbinian <korbiniank@outlook.de>
2024-11-07 23:59:00 +0100

    add users position to leaderboard

--- src/lib/domains/leaderboard/adapter/UserLeaderboardAdapter.ts
+++ src/lib/domains/leaderboard/adapter/UserLeaderboardAdapter.ts
@@ -21,4 +21,27 @@ export class UserLeaderboardAdapter {
 
     return response.data.data;
   }
+
+  /**
+   *  Fetches the data for a single user from the /leaderboard/user endpoint
+   *
+   * @param {number} season
+   * @param {string} address
+   * @return {*}  {Promise<PaginationInfo<UserLeaderboardItem>>}
+   * @memberof UserLeaderboardAdapter
+   */
+  async fetchLeaderboardPositionForAddress(
+    season: number,
+    address: string,
+  ): Promise<PaginationInfo<UserLeaderboardItem>> {
+    log('fetching user leaderboard data', season, address);
+    const client = getAxiosInstance(season);
+    const response = await client.get('/v2/leaderboard/user', {
+      ...globalAxiosConfig,
+      params: { address },
+    });
+    log('userLeaderboardData', response.data.data);
+
+    return response.data.data;
+  }
 }
