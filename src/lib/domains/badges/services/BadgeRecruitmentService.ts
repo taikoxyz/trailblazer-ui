caf4c659151be73ce01759960eb053343a0148c2
bearni95 <bernatcanal@gmail.com>
2024-11-27 17:36:45 +0100

    docs for badge recruitment service

--- src/lib/domains/badges/services/BadgeRecruitmentService.ts
+++ src/lib/domains/badges/services/BadgeRecruitmentService.ts
@@ -16,17 +16,35 @@ export class BadgeRecruitmentService {
   constructor(recruitmentAdapter?: BadgeRecruitmentAdapter) {
     this.adapter = recruitmentAdapter || new BadgeRecruitmentAdapter();
   }
-
+  /**
+   * Fetches the system's enabled migrations
+   * @return {*}  {Promise<number[]>}
+   */
   async getEnabledRecruitments(): Promise<number[]> {
     log('getEnabledRecruitments');
     return this.adapter.fetchEnabledRecruitments();
   }
 
+  /**
+   * Initiate a recruitment (migration) process
+   * @param address
+   * @param nft
+   * @param recruitment
+   * @returns {*} {Promise<IBadgeRecruitment>}
+   */
   async startRecruitment(address: Address, nft: NFT, recruitment: IBadgeRecruitment): Promise<IBadgeRecruitment> {
     log('startRecruitment', { address, nft, recruitment });
     return this.adapter.startRecruitment(address, nft, recruitment);
   }
 
+  /**
+   * Influence (tamper) the recruitment process
+   * @param address
+   * @param nft
+   * @param selectedMovement
+   * @param recruitment
+   * @returns {*} {Promise<IBadgeRecruitment>}
+   * */
   async influenceRecruitment(
     address: Address,
     nft: NFT,
@@ -37,11 +55,23 @@ export class BadgeRecruitmentService {
     return this.adapter.influenceRecruitment(address, nft, selectedMovement, recruitment);
   }
 
+  /**
+   * End (claim) the recruitment process
+   * @param address
+   * @param nft
+   * @param recruitment
+   * @returns {*} {Promise<IBadgeRecruitment>}
+   */
   async endRecruitment(address: Address, nft: NFT, recruitment: IBadgeRecruitment): Promise<IBadgeRecruitment> {
     log('endRecruitment', { address, nft, recruitment });
     return this.adapter.endRecruitment(address, nft, recruitment);
   }
 
+  /**
+   * Get the recruitment status for a particular user
+   * @param address
+   * @returns {*} {Promise<IBadgeRecruitment[]>}
+   */
   async getRecruitmentStatus(address: Address): Promise<IBadgeRecruitment[]> {
     log('getRecruitmentStatus', { address });
     const recruitments = await this.adapter.getRecruitmentStatus(address);
@@ -56,6 +86,11 @@ export class BadgeRecruitmentService {
     return recruitments as IBadgeRecruitment[];
   }
 
+  /**
+   * Fetch the max amount of influences allowed
+   * @param exp
+   * @returns
+   */
   async getMaxInfluences(exp: number): Promise<number> {
     log('getMaxInfluences', { exp });
     return this.adapter.getMaxInfluences(exp);
