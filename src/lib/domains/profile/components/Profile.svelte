28bd11b335c5b65ba0c8e2664c88c57662867624
Korbinian <korbiniank@outlook.de>
2024-11-28 23:07:00 +0100

    lint

--- src/lib/domains/profile/components/Profile.svelte
+++ src/lib/domains/profile/components/Profile.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { type Address,getAddress } from 'viem';
+  import { getAddress, type Address } from 'viem';
 
   import { page } from '$app/stores';
   import BoosterCard from '$lib/domains/profile/components/BoosterCard.svelte';
