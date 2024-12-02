71f199c37f0c6c9b97ceb2dc9bbc61e551df684d
bearni95 <bernatcanal@gmail.com>
2024-12-02 12:23:28 +0100

    added bubbles to s1 and s2 nfts

---
+++ src/lib/domains/profile/components/ProfileNFTs/MultiplierBadge.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Icon } from '$shared/components/Icon';
+  import type { NFT } from '$shared/types/NFT';
+  import { classNames } from '$shared/utils/classNames';
+  import getMultiplierFor from '$shared/utils/nfts/getMultiplierFor';
+
+  export let token: NFT;
+
+  $: multiplierInfo = getMultiplierFor(token);
+  $: multiplier = multiplierInfo?.multiplier || 1;
+  $: tooltip = multiplierInfo?.tooltip || '';
+
+  const wrapperClasses = classNames(
+    'border',
+    'border-pink-50',
+    'rounded-full',
+    'px-[10px]',
+    'py-[5px]',
+    'flex',
+    'flex-row',
+    'justify-center',
+    'items-center',
+    'gap-[5px]',
+    'multiplier-background',
+    'relative',
+    'tooltip',
+    'tooltip-bottom',
+  );
+
+  const borderOverlayClasses = classNames(
+    'w-full',
+    'h-[1px]',
+    'bg-gradient-to-r from-transparent via-primary to-transparent',
+    'absolute',
+  );
+</script>
+
+<div class={wrapperClasses} data-tip={tooltip}>
+  {multiplier}x Multiplier
+  <Icon type="question-circle" />
+
+  <div class={classNames(borderOverlayClasses, 'top-[-1px]')}></div>
+  <div class={classNames(borderOverlayClasses, 'bottom-[-1px]')}></div>
+</div>
---
+++ src/lib/shared/utils/nfts/getMultiplierFor.ts
@@ -0,0 +1,46 @@
+import { isAddressEqual } from 'viem';
+
+import { taikoonTokenAddress, trailblazersBadgesAddress, trailblazersBadgesS2Address } from '$generated/abi';
+import { Movements, Seasons } from '$lib/domains/profile/types/types';
+import type { NFT } from '$shared/types/NFT';
+
+import { chainId } from '../chain';
+
+export default function getMultiplierFor(token: NFT): {
+  multiplier: number;
+  tooltip: string;
+} {
+  if (isAddressEqual(token.address, taikoonTokenAddress[chainId])) {
+    // Taikoon
+    return {
+      multiplier: 1,
+      tooltip: 'Applied globally',
+    };
+  }
+
+  if (isAddressEqual(token.address, trailblazersBadgesAddress[chainId]) || token.metadata.season === Seasons.Season1) {
+    // Trailblazers Badges, season 1
+    return {
+      multiplier: 0.5,
+      tooltip: 'Global and Proposer XP',
+    };
+  }
+
+  if (
+    isAddressEqual(token.address, trailblazersBadgesS2Address[chainId]) ||
+    token.metadata.season === Seasons.Season2
+  ) {
+    // Trailblazers Badges, season 2
+    const movement = token.metadata.movement as Movements;
+    return {
+      multiplier: 0.5,
+      tooltip: movement === Movements.Minnow ? 'Transaction Count XP' : 'Transaction Value XP',
+    };
+  }
+
+  // Default (Snaefell)
+  return {
+    multiplier: 0.1,
+    tooltip: 'Applied globally',
+  };
+}
