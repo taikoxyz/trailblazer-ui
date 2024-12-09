f41530c88a35f90f886890fbd36f5e7b3847d705
Korbinian <korbiniank@outlook.de>
2024-12-10 00:28:06 +0100

    prep s3 leaderboards

--- src/lib/shared/services/api/axiosClient.ts
+++ src/lib/shared/services/api/axiosClient.ts
@@ -9,13 +9,15 @@ import { isDevelopmentEnv } from '$shared/utils/isDevelopmentEnv';
 
 const baseApiUrl = isDevelopmentEnv ? '/api/mock-api' : PUBLIC_TRAILBLAZER_API_URL;
 
-export const baseSeasonURL = `${baseApiUrl}/s${get(activeSeason)}`;
+const getBaseSeasonURL = (season: number = get(activeSeason)) => {
+  return `${baseApiUrl}/s${season}`;
+};
 
 export const getSeasonApiUrl = (season: number = get(activeSeason)) => {
   if (season === 1) {
     return baseApiUrl;
   } else {
-    return baseSeasonURL;
+    return getBaseSeasonURL(season);
   }
 };
 
---
+++ src/routes/leaderboard/s3/user/+page.ts
@@ -0,0 +1,38 @@
+import { browser } from '$app/environment';
+import { leaderboardConfig } from '$config';
+import { userLeaderboardService } from '$lib/domains/leaderboard/services/LeaderboardServiceInstances';
+import { currentUserLeaderboardUserEntry } from '$lib/domains/leaderboard/stores/userLeaderboard';
+import type { UserLeaderboardItem } from '$lib/domains/leaderboard/types/user/types';
+import type { PaginationInfo } from '$lib/shared/dto/CommonPageApiResponse';
+import getConnectedAddress from '$shared/utils/getConnectedAddress';
+
+export const load = async () => {
+  let loading = true;
+  let pageInfo: PaginationInfo<UserLeaderboardItem> = {
+    page: 0,
+    size: leaderboardConfig.pageSize,
+    first: 0,
+    last: 1,
+    total: 0,
+  };
+
+  if (browser) {
+    try {
+      const page = await userLeaderboardService.getUserLeaderboardData(pageInfo, 3);
+      currentUserLeaderboardUserEntry.set(
+        await userLeaderboardService.getUserLeaderboardDataForAddress(3, getConnectedAddress()),
+      );
+      if (page) {
+        pageInfo = page.pagination;
+      }
+    } catch (error) {
+      console.error('Error loading leaderboard data:', error);
+    } finally {
+      loading = false;
+    }
+  }
+  return {
+    pageInfo,
+    loading,
+  };
+};
