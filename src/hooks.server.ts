80681f396722533540f30678eccb7c4c38f0c544
Korbinian <korbiniank@outlook.de>
2024-12-10 01:20:52 +0100

    add bypass for crawlers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -55,14 +55,49 @@ const bannedCountries: Record<string, string> = {
   ZW: 'Zimbabwe',
 };
 
+const allowedUserAgents = [
+  'Googlebot',
+  'Googlebot-Image',
+  'Googlebot-Video',
+  'Googlebot-News',
+  'Mediapartners-Google',
+  'AdsBot-Google',
+  'APIs-Google',
+  'Google-Read-Aloud',
+  'Google-Firebase-Cloud-Messaging',
+  'DuplexWeb-Google',
+  'Bingbot',
+  'DuckDuckBot',
+  'Baiduspider',
+  'YandexBot',
+  'Sogou',
+  'Exabot',
+  'facebot',
+  'ia_archiver', // Alexa's crawler
+  'Twitterbot',
+  'Applebot',
+  'LinkedInBot',
+  'Pinterestbot',
+  'CCBot', // Common Crawl
+  'GPTBot', // OpenAI's crawler
+  'PerplexityBot',
+  'ImagesiftBot',
+  'Diffbot',
+  'Google-Extended',
+  'GoogleOther',
+];
+
 const bannedCountryCodes = Object.keys(bannedCountries);
 
 export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
-  const country = event.request.headers.get('x-vercel-ip-country') ?? false;
+  const country = event.request.headers.get('x-vercel-ip-country') ?? '';
+  const userAgent = event.request.headers.get('user-agent') ?? '';
   const isDev = event.url.hostname === 'localhost' || event.url.port === '5173';
   const isBypassed = PUBLIC_BYPASS_GEOBLOCK === 'true';
 
-  const allowed = isDev || isBypassed || (country && !bannedCountryCodes.includes(country));
+  const isKnownCrawler = allowedUserAgents.some((crawler) => userAgent.includes(crawler));
+
+  const allowed = isDev || isBypassed || isKnownCrawler || (country && !bannedCountryCodes.includes(country));
 
   event.locals.allowed = Boolean(allowed);
 
