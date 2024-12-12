64cff3b35f9fb408a691d73880438194493330d1
Korbinian <korbiniank@outlook.de>
2024-12-12 15:53:00 +0100

    extract css

--- src/lib/domains/profile/components/ProfileMultiplierCard/MultiplierCard.svelte
+++ src/lib/domains/profile/components/ProfileMultiplierCard/MultiplierCard.svelte
@@ -19,9 +19,7 @@
 
   onMount(async () => {
     const mp: Multipliers = await profileService.getBadgeMultiplier(getConnectedAddress(), $activeSeason);
-
     const { globalMultiplier, transactionVolumeMultiplier, transationMultiplier } = mp.multipliers;
-
     globalMultiplierView = globalMultiplier;
     transactionVolumeMultiplierView = transactionVolumeMultiplier;
     transationMultiplierView = transationMultiplier;
@@ -35,16 +33,24 @@
     'h-[66px]',
     'w-full',
     'left-0',
-    'top-0 ',
+    'top-0',
     'gap-[5px]',
   );
+
+  const cardClasses = classNames('bg-purple-900', 'relative', 'flex', 'flex-col', 'h-full');
+  const statusClasses = classNames('flex-1', 'mt-[65px]');
+  const fontBoldTextLeftClasses = classNames('font-bold', 'text-left');
+  const listClasses = classNames('f-col', 'gap-[8px]');
+  const ctaClasses = classNames('absolute', 'bottom-0', 'w-full', 'pb-[27px]', 'px-[24px]');
+  const linkClasses = classNames('f-row', 'justify-between', 'items-center', 'w-full');
+  const tooltipContentClasses = classNames('bg-white', 'text-black');
 </script>
 
-<AbstractProfileCard class="bg-purple-900 relative flex flex-col h-full">
+<AbstractProfileCard class={cardClasses}>
   <div slot="title" class={headerClasses}>
     <span class="text-center font-bold">{$t('booster.title')}</span>
     <Tooltip slot="tooltip" position="bottom">
-      <div class="bg-white text-black">
+      <div class={tooltipContentClasses}>
         <h2 class="text-black">{$t('booster.tooltip.title')}</h2>
         <div class="body-regular text-black">
           {$t('booster.tooltip.description')}
@@ -53,9 +59,9 @@
     </Tooltip>
   </div>
 
-  <div slot="status" class="flex-1 mt-[65px]">
-    <div class="font-bold text-left">
-      <ul class="f-col gap-[8px]">
+  <div slot="status" class={statusClasses}>
+    <div class={fontBoldTextLeftClasses}>
+      <ul class={listClasses}>
         <MultiplierItem label={$t('profile.multipliers.card.tx')} multiplierView={transationMultiplierView} />
         <MultiplierItem
           label={$t('profile.multipliers.card.txValue')}
@@ -65,8 +71,8 @@
     </div>
   </div>
 
-  <div slot="cta" class="absolute bottom-0 w-full pb-[27px] px-[24px]">
-    <a class="f-row justify-between items-center w-full" href={`/profile/${getConnectedAddress()}#badge-recruitment`}>
+  <div slot="cta" class={ctaClasses}>
+    <a class={linkClasses} href={`/profile/${getConnectedAddress()}#badge-recruitment`}>
       <span class="font-bold">{$t('profile.multipliers.card.cta')}</span>
       <Icon type="chevron-right" />
     </a>
