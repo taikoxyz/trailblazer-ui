6c55bb60425e0ade2d6b64d19c851579dd18b95e
Korbinian <korbiniank@outlook.de>
2025-01-02 01:41:17 +0100

    prerender test

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-
+export const prerender = false;
 export function GET() {
   return json({ health: true });
 }
--- src/routes/journeys/+page.server.ts
+++ src/routes/journeys/+page.server.ts
@@ -2,6 +2,8 @@ import { redirect } from '@sveltejs/kit';
 
 import type { PageServerLoad } from './$types';
 
+export const prerender = true;
+
 export const load: PageServerLoad = async () => {
   throw redirect(302, '/');
 };
