7d53a4aacb71f31787716873e7a6352638eed702
Korbinian <korbiniank@outlook.de>
2025-01-07 13:59:09 +0100

    basic scaffold

--- src/lib/domains/profile/components/ProfileTabs.svelte
+++ src/lib/domains/profile/components/ProfileTabs.svelte
@@ -11,6 +11,7 @@
 
   import ProfileTransactions from './ProfileActivity/ProfileTransactions.svelte';
   import ProfileLockdownTab from './ProfileLockdownTab.svelte';
+  import ProfileNftCollection from './ProfileNFTs/NFTCollection/ProfileNFTCollection.svelte';
   import ProfileRewardClaim from './ProfileRewardClaim/ProfileRewardClaim.svelte';
 
   type TabContent = {
@@ -35,7 +36,7 @@
     {
       slug: 'nft-collection',
       name: 'NFT Collection',
-      content: ProfileNFTs,
+      content: ProfileNftCollection,
     },
     ...(isSelfProfile
       ? [
