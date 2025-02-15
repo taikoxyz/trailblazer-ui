96ce0b2fd5969964fd77055577114d7f4fa2e892
Korbinian <korbiniank@outlook.de>
2025-02-15 21:47:53 +0100

    remove cards

--- src/lib/shared/components/FeaturedCampaigns/cards.ts
+++ src/lib/shared/components/FeaturedCampaigns/cards.ts
@@ -2,36 +2,6 @@ import type { CampaignCardType } from './types';
 
 export const cards: CampaignCardType[] = [
   {
-    title: 'Claim your 50K points now!',
-    text: 'Vote for your favorite projects on DoraHacks. Don’t miss out—claim your 50,000 points today!',
-    btn: {
-      text: 'Vote now',
-      external: true,
-      destination: 'https://dorahacks.io/grant/taiko/buidl',
-    },
-    images: {
-      sm: '/campaign/cards/sm/dorahacks.png',
-      md: '/campaign/cards/xl/dorahacks.png',
-      lg: '/campaign/cards/xl/dorahacks.png',
-      xl: '/campaign/cards/xl/dorahacks.png',
-    },
-  },
-  {
-    title: 'Avalon x Taiko: Exclusive rewards await',
-    text: 'Supply TAIKO on Avalon to unlock airdrops, 100K TAIKO incentives, and 100x Trailblazers XP! ⏳',
-    btn: {
-      text: 'Earn now',
-      external: true,
-      destination: 'https://usdalend.avalonfinance.xyz/?marketName=proto_usda_taiko_v3',
-    },
-    images: {
-      sm: '/campaign/cards/sm/avalon.png',
-      md: '/campaign/cards/sm/avalon.png',
-      lg: '/campaign/cards/sm/avalon.png',
-      xl: '/campaign/cards/sm/avalon.png',
-    },
-  },
-  {
     title: 'Evolve your Faction Badges',
     text: 'The Devs are out in full force looking for new recruits to join their Faction. Will you join them?',
     btn: {
