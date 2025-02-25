dd23d356b5f44cbfed7d5f4ea795d76d40c0e4e1
bearni95 <bernatcanal@gmail.com>
2025-02-25 12:17:07 +0100

    restores getNftImage

--- src/lib/shared/utils/nfts/getNftImage.ts
+++ src/lib/shared/utils/nfts/getNftImage.ts
@@ -1,17 +1,5 @@
-import { isAddressEqual } from 'viem';
-
-import { taikoonTokenAddress } from '$generated/abi';
 import type { NFT } from '$shared/types/NFT';
 
-import { chainId } from '../chain';
-
 export default function getNftImage(nft: NFT) {
-  if (isAddressEqual(nft.address, taikoonTokenAddress[chainId])) {
-    const image = nft.metadata.image as string;
-    if (image.startsWith('http') || image.startsWith('/')) {
-      return image;
-    }
-    return `https://ipfs.io/ipfs/${image}`;
-  }
   return nft.metadata.image as string;
 }
