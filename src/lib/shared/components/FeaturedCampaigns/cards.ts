450cd4783a9b997fdb7a33f4fce591079814541a
Korbinian <korbiniank@outlook.de>
2025-03-16 00:14:40 +0100

    update cards

--- src/lib/shared/components/FeaturedCampaigns/cards.ts
+++ src/lib/shared/components/FeaturedCampaigns/cards.ts
@@ -2,22 +2,7 @@ import type { CampaignCardType } from './types';
 
 export const cards: CampaignCardType[] = [
   {
-    title: 'TAKOTAKO Tales x Taiko',
-    text: 'Score 8,888 TAIKO and a Taikoon. Join the TAKOTAKO Galxe campaign and complete tasks.',
-    btn: {
-      text: 'Join now',
-      external: true,
-      destination: `https://app.galxe.com/quest/VnWkqAtB2Row8KdwpFHW9n/GC6MbtpuTp`,
-    },
-    images: {
-      sm: '/campaign/cards/sm/takotako.png',
-      md: '/campaign/cards/xl/takotako.png',
-      lg: '/campaign/cards/xl/takotako.png',
-      xl: '/campaign/cards/xl/takotako.png',
-    },
-  },
-  {
-    title: 'Evolve your Faction Badges',
+    title: 'New look, who ‘dis?',
     text: 'The Devs are out in full force looking for new recruits to join their Faction. Will you join them?',
     btn: {
       text: 'Recruit now',
@@ -25,19 +10,34 @@ export const cards: CampaignCardType[] = [
       destination: `profile/`,
     },
     images: {
-      sm: '/campaign/cards/sm/recruit.png',
-      md: '/campaign/cards/xl/recruit.png',
-      lg: '/campaign/cards/xl/recruit.png',
-      xl: '/campaign/cards/xl/recruit.png',
+      sm: '/campaign/cards/sm/newNft.png',
+      md: '/campaign/cards/xl/newNft.png',
+      lg: '/campaign/cards/xl/newNft.png',
+      xl: '/campaign/cards/xl/newNft.png',
     },
   },
   {
-    title: 'Liquidity Royale Season 3',
-    text: 'Supercharge your liquidity and get rewarded for it. Season 3 is offering a massive 1.2M TAIKO tokens!',
+    title: 'Blaze new trails with Thrillblazer S4',
+    text: 'Blaze new trails, fuel Taiko’s momentum, and seize your chance for legendary rewards.',
     btn: {
       text: 'Earn now',
       external: false,
-      destination: 'journeys/liquidity',
+      destination: 'journeys/thrillblazer/4',
+    },
+    images: {
+      sm: '/campaign/cards/sm/thrillblazer.png',
+      md: '/campaign/cards/xl/thrillblazer.png',
+      lg: '/campaign/cards/xl/thrillblazer.png',
+      xl: '/campaign/cards/xl/thrillblazer.png',
+    },
+  },
+  {
+    title: 'Liquidity Royale Season 4',
+    text: 'Compete, conquer and earn! 1 Million TAIKO tokens are up for grabs!',
+    btn: {
+      text: 'Earn now',
+      external: false,
+      destination: 'journeys/liquidity/3',
     },
     images: {
       sm: '/campaign/cards/sm/liquidity-royale.png',
