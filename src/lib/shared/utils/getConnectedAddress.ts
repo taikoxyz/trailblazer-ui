732946845399d67fd4d3e2df6b65421f714463b0
bearni95 <bernatcanal@gmail.com>
2025-04-03 03:21:08 +0200

    fixes

--- src/lib/shared/utils/getConnectedAddress.ts
+++ src/lib/shared/utils/getConnectedAddress.ts
@@ -13,5 +13,5 @@ export default function getConnectedAddress(): Address {
   */
   //return '0x4b2Cb0dfB311eba47739BF47A5c4Ae5934671060'
 
-  return '0x8ad557e25766c99e28e6fe8e3e0fbf87e452b23d';
+  return '0xdbc902a240942c64fa52a7a0e2020a4cab3b33a3';
 }
