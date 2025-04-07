35cd9e019816b0691dbaff16efc561c6dfedd124
Korbinian <korbiniank@outlook.de>
2025-04-07 15:49:08 +0200

    more checks

--- src/lib/domains/leaderboard/components/Competition/DappCompetition/Thrillblazer/thrillblazerDetails.ts
+++ src/lib/domains/leaderboard/components/Competition/DappCompetition/Thrillblazer/thrillblazerDetails.ts
@@ -5,128 +5,137 @@ import { setupI18n } from '$i18n/index';
 
 import type { CompetitionInfo } from '../../types';
 
-// Ensure i18n is initialized before accessing translations
-await setupI18n();
+export let thrillblazerDetails: Record<number, CompetitionInfo> | null = null;
 
-export const thrillblazerDetails: Record<number, CompetitionInfo> = {
-  1: {
-    title: '',
-    description: '',
-    prizeTitle: get(t)('leaderboard.gaming.prize.1'),
-    prizeSubtitle: '',
-    prizes: [
-      {
-        image: '/first.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.first.amount'),
-      },
-      {
-        image: '/second.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.second.amount'),
-      },
-      {
-        image: '/third.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.third.amount'),
-      },
-      {
-        image: '/default-prize.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.fourth.amount'),
-      },
-      {
-        image: '/default-prize.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.fifth.amount'),
-      },
-    ],
-    qualifyingPositions: 5,
-  } satisfies CompetitionInfo,
-  2: {
-    title: '',
-    description: '',
-    prizeTitle: get(t)('leaderboard.gaming.prize.1'),
-    prizeSubtitle: '',
-    prizes: [
-      {
-        image: '/first.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.first.amount'),
-      },
-      {
-        image: '/second.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.second.amount'),
-      },
-      {
-        image: '/third.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.third.amount'),
-      },
-      {
-        image: '/default-prize.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.fourth.amount'),
-      },
-    ],
-    qualifyingPositions: 4,
-  } satisfies CompetitionInfo,
-  3: {
-    title: '',
-    description: '',
-    prizeTitle: get(t)('leaderboard.gaming.prize.1'),
-    prizeSubtitle: '',
-    prizes: [
-      {
-        image: '/first.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.first.amount'),
-      },
-      {
-        image: '/second.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.second.amount'),
-      },
-      {
-        image: '/third.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.third.amount'),
-      },
-      {
-        image: '/default-prize.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.fourth.amount'),
-      },
-      {
-        image: '/default-prize.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.fifth.amount'),
-      },
-    ],
-    qualifyingPositions: 5,
-  } satisfies CompetitionInfo,
-  4: {
-    title: '',
-    description: '',
-    prizeTitle: get(t)('leaderboard.gaming.prize.1'),
-    prizeSubtitle: '',
-    prizes: [
-      {
-        image: '/thrillblazers/prize/first.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.first.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/second.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.second.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/third.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.third.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/default.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.fourth.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/default.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.fifth.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/default.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.sixth.amount'),
-      },
-      {
-        image: '/thrillblazers/prize/default.svg',
-        amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.seventh.amount'),
-      },
-    ],
-    qualifyingPositions: 7,
-  },
+const thrillblazerDetailsPromise = (async () => {
+  await setupI18n();
+  thrillblazerDetails = {
+    1: {
+      title: '',
+      description: '',
+      prizeTitle: get(t)('leaderboard.gaming.prize.1'),
+      prizeSubtitle: '',
+      prizes: [
+        {
+          image: '/first.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.first.amount'),
+        },
+        {
+          image: '/second.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.second.amount'),
+        },
+        {
+          image: '/third.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.third.amount'),
+        },
+        {
+          image: '/default-prize.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.fourth.amount'),
+        },
+        {
+          image: '/default-prize.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition1.prize_breakdown.fifth.amount'),
+        },
+      ],
+      qualifyingPositions: 5,
+    },
+    2: {
+      title: '',
+      description: '',
+      prizeTitle: get(t)('leaderboard.gaming.prize.1'),
+      prizeSubtitle: '',
+      prizes: [
+        {
+          image: '/first.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.first.amount'),
+        },
+        {
+          image: '/second.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.second.amount'),
+        },
+        {
+          image: '/third.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.third.amount'),
+        },
+        {
+          image: '/default-prize.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition2.prize_breakdown.fourth.amount'),
+        },
+      ],
+      qualifyingPositions: 4,
+    },
+    3: {
+      title: '',
+      description: '',
+      prizeTitle: get(t)('leaderboard.gaming.prize.1'),
+      prizeSubtitle: '',
+      prizes: [
+        {
+          image: '/first.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.first.amount'),
+        },
+        {
+          image: '/second.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.second.amount'),
+        },
+        {
+          image: '/third.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.third.amount'),
+        },
+        {
+          image: '/default-prize.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.fourth.amount'),
+        },
+        {
+          image: '/default-prize.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition3.prize_breakdown.fifth.amount'),
+        },
+      ],
+      qualifyingPositions: 5,
+    },
+    4: {
+      title: '',
+      description: '',
+      prizeTitle: get(t)('leaderboard.gaming.prize.1'),
+      prizeSubtitle: '',
+      prizes: [
+        {
+          image: '/thrillblazers/prize/first.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.first.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/second.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.second.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/third.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.third.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/default.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.fourth.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/default.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.fifth.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/default.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.sixth.amount'),
+        },
+        {
+          image: '/thrillblazers/prize/default.svg',
+          amount: get(t)('leaderboard.thrillblazers.edition4.prize_breakdown.seventh.amount'),
+        },
+      ],
+      qualifyingPositions: 7,
+    },
+  };
+})();
+
+export const getThrillblazerDetails = async () => {
+  if (!thrillblazerDetails) {
+    await thrillblazerDetailsPromise;
+  }
+  return thrillblazerDetails;
 };
