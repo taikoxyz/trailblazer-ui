0ad9b432751d191c82192a727b32848624c1e8d5
Korbinian <korbiniank@outlook.de>
2025-04-25 13:06:43 +0200

    update button states

--- src/lib/domains/profile/components/ProfileSeasonBonusCard/FloatingClaimButton.svelte
+++ src/lib/domains/profile/components/ProfileSeasonBonusCard/FloatingClaimButton.svelte
@@ -3,7 +3,6 @@
   import { t } from 'svelte-i18n';
   import { type Address, isAddressEqual, zeroAddress } from 'viem';
 
-  import { browser } from '$app/environment';
   import { PUBLIC_SEASON_BONUS_CLAIM_ACTIVE } from '$env/static/public';
   import ActionButton from '$shared/components/Button/ActionButton.svelte';
   import { activeSeason } from '$shared/stores/activeSeason';
@@ -30,65 +29,50 @@
     'pl-[16px]',
     'gap-[40px]',
   );
-  const wrapperNotActiveClasses = classNames(
-    'bg-grey-700',
-    'fixed',
-    'bottom-[30px]',
-    'right-[30px]',
-    'flex',
-    'justify-center',
-    'items-center',
-    'rounded-full',
-    'shadow-[0px_0px_30px_0px_#FFC6E933]',
-    'border',
-    'border-grey-600',
-    'p-[12px]',
-    'pl-[16px]',
-    'pr-[16px]',
-  );
 
   const labelClasses = classNames('text-grey-10', 'font-[700]', 'text-[16px]/[24px]', 'flex');
 
   const iconClasses = classNames('w-[24px]', 'h-[24px]');
 
-  $: visible = false;
-  function load() {
-    const connectedAddress = getConnectedAddress();
-    if (!browser || !connectedAddress || connectedAddress === zeroAddress) {
-      return;
-    }
+  const checkButton = async () => {
     const urlAddress = window.location.pathname.split('/')[2];
-    visible = isAddressEqual(connectedAddress, urlAddress as Address);
-  }
-
-  $: getConnectedAddress() && load();
-  $: claimActive = false;
-
-  $: bonusClaimActive = PUBLIC_SEASON_BONUS_CLAIM_ACTIVE === 'true';
-  onMount(async () => {
-    const address = getConnectedAddress();
-    if (address && address !== zeroAddress && $activeSeason && bonusClaimActive) {
-      claimActive = await profileService.checkRegistrationOpen(EventIds.SEASON2);
+    const connectedAddress = getConnectedAddress();
+    if (
+      connectedAddress &&
+      connectedAddress !== zeroAddress &&
+      $activeSeason &&
+      PUBLIC_SEASON_BONUS_CLAIM_ACTIVE === 'true' &&
+      isAddressEqual(connectedAddress, urlAddress as Address)
+    ) {
+      // button is visible if connected address is the same as the visited profile and env is set to true
+      isButtonVisible = true;
+      const claimActive = await profileService.checkRegistrationOpen(EventIds.SEASON2);
+      // button is active if the claim is open on contract side
+      isClaimActive = claimActive;
     }
+  };
+
+  $: getConnectedAddress() && checkButton();
+  $: isButtonVisible = false;
+  $: isClaimActive = false;
+
+  onMount(async () => {
+    await checkButton();
   });
 </script>
 
-{#if visible}
-  {#if claimActive}
-    <div style="z-index:100;" class={wrapperActiveClasses}>
-      <div class={labelClasses}>
-        <img class={iconClasses} src="/news/flame.svg" alt="Flame" />
+{#if isButtonVisible}
+  <div style="z-index:100;" class={wrapperActiveClasses}>
+    <div class={labelClasses}>
+      <img class={iconClasses} src="/news/flame.svg" alt="Flame" />
+      {#if isClaimActive}
         {$t('claim.modal.float_cta_open')}
-      </div>
-      <ActionButton onPopup disabled={!claimActive} on:click={() => claimModal.set(true)} priority="primary"
-        >Claim Now</ActionButton>
-    </div>
-  {:else}
-    <div style="z-index:100;" class={wrapperNotActiveClasses}>
-      <div class={labelClasses}>
-        <img class={iconClasses} src="/news/flame.svg" alt="Flame" />
+      {:else}
         {$t('claim.modal.float_cta_closed')}
-      </div>
+      {/if}
     </div>
-  {/if}
+    <ActionButton onPopup disabled={!isClaimActive} on:click={() => claimModal.set(true)} priority="primary">
+      Claim now
+    </ActionButton>
+  </div>
 {/if}
