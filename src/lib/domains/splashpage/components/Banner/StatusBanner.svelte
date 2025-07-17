c7b58498c17e76fa8db5c4d56094cf58336b80a8
Korbinian <korbiniank@outlook.de>
2025-07-17 10:18:16 +0200

    add status banner and empty card

---
+++ src/lib/domains/splashpage/components/Banner/StatusBanner.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { classNames } from '$shared/utils/classNames';
+
+  const imageClasses = classNames(
+    'bg-[url(/banner/statusbanner/sm.png)]',
+    'md:bg-[url(/banner/statusbanner/md.png)]',
+    'lg:bg-[url(/banner/statusbanner/lg.png)]',
+    'xl:bg-[url(/banner/statusbanner/xl.png)]',
+    'w-full',
+    'h-[732px]',
+    'md:h-[427px]',
+    'lg:h-[300px]',
+    'xl:h-[250px]',
+    'bg-contain',
+    'bg-no-repeat',
+    'bg-center',
+    'cursor-pointer',
+  );
+</script>
+
+<div class={imageClasses} />
